36ea8e8bbff38f693b512433b00b9d90c1e09266
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-10 20:53:58 +1100

    Add shorthands for Vector3

--- FinalEngine.Maths/Vector3.cs
+++ FinalEngine.Maths/Vector3.cs
@@ -4,6 +4,30 @@
 
     public struct Vector3 : IEquatable<Vector3>
     {
+        public static readonly Vector3 Back = new Vector3(0, 0, -1);
+
+        public static readonly Vector3 Down = new Vector3(0, -1, 0);
+
+        public static readonly Vector3 Forward = new Vector3(0, 0, 1);
+
+        public static readonly Vector3 Left = new Vector3(-1, 0, 0);
+
+        public static readonly Vector3 NegativeInfinity = new Vector3(float.NegativeInfinity,
+                                                                      float.NegativeInfinity,
+                                                                      float.NegativeInfinity);
+
+        public static readonly Vector3 One = new Vector3(1, 1, 1);
+
+        public static readonly Vector3 PositiveInfinity = new Vector3(float.PositiveInfinity,
+                                                                      float.PositiveInfinity,
+                                                                      float.PositiveInfinity);
+
+        public static readonly Vector3 Right = new Vector3(1, 0, 0);
+
+        public static readonly Vector3 Up = new Vector3(0, 1, 0);
+
+        public static readonly Vector3 Zero = new Vector3(0, 0, 0);
+
         public Vector3(float x, float y, float z)
         {
             X = x;

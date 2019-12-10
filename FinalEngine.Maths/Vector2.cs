e532387bb5f7f7da63a4b23a6ab7d126413c52bb
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-10 16:19:07 +1100

    Create Vector2

---
+++ FinalEngine.Maths/Vector2.cs
@@ -0,0 +1,43 @@
+﻿namespace FinalEngine.Maths
+{
+    using System;
+
+    public struct Vector2 : IEquatable<Vector2>
+    {
+        public Vector2(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Vector2))
+            {
+                return false;
+            }
+
+            return Equals((Vector2)obj);
+        }
+
+        public bool Equals(Vector2 other)
+        {
+            return X == other.X &&
+                   Y == other.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return new { X, Y }.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}

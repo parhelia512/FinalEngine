ffa7fb486b9a4641f846e111af1a20c01efb0cad
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-10 20:40:00 +1100

    Create Matrix4 and added unit tests

---
+++ FinalEngine.Maths.Tests/Matrix4Tests.cs
@@ -0,0 +1,168 @@
+﻿namespace FinalEngine.Maths.Tests
+{
+    using NUnit.Framework;
+
+    public sealed class Matrix4Tests
+    {
+        [Test]
+        public void Equals_Operator_Test_Should_Return_False_When_Matrix4_Does_Not_Contain_Same_Property_Values()
+        {
+            // Arrange
+            var left = new Matrix4(new Vector4(1, 0, 3, 4),
+                                   new Vector4(0, 1, 4, 3),
+                                   new Vector4(1, 0, 2, 1),
+                                   new Vector4(0, 3, 4, 5));
+
+            var right = new Matrix4(new Vector4(0, 1, 4, 5),
+                                    new Vector4(1, 0, 7, 3),
+                                    new Vector4(4, 5, 6, 1),
+                                    new Vector4(4, 5, 6, 7));
+
+            // Act
+            bool result = left == right;
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Equals_Operator_Test_Should_Return_True_When_Matrix4_Contains_Same_Property_Values()
+        {
+            // Arrange
+            var left = new Matrix4(new Vector4(1, 0, 3, 4),
+                                   new Vector4(0, 1, 5, 55),
+                                   new Vector4(34, 56, 76, 29),
+                                   new Vector4(45, 66, 123, 4573));
+
+            var right = new Matrix4(new Vector4(1, 0, 3, 4),
+                                   new Vector4(0, 1, 5, 55),
+                                   new Vector4(34, 56, 76, 29),
+                                   new Vector4(45, 66, 123, 4573));
+
+            // Act
+            bool result = left == right;
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Equals_Test_Should_Return_False_When_Matrix4_Does_Not_Contain_Same_Property_Values()
+        {
+            // Arrange
+            var left = new Matrix4(new Vector4(1, 0, 3, 4),
+                                   new Vector4(0, 1, 4, 3),
+                                   new Vector4(1, 0, 2, 1),
+                                   new Vector4(0, 3, 4, 5));
+
+            var right = new Matrix4(new Vector4(0, 1, 4, 5),
+                                    new Vector4(1, 0, 7, 3),
+                                    new Vector4(4, 5, 6, 1),
+                                    new Vector4(4, 5, 6, 7));
+
+            // Act
+            bool result = left.Equals(right);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Equals_Test_Should_Return_False_When_Not_Matrix4()
+        {
+            // Arrange
+            var Matrix4 = new Matrix4();
+
+            // Act
+            bool result = Matrix4.Equals(new object());
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Equals_Test_Should_Return_True_When_Matrix4_Does_Contain_Same_Property_Values()
+        {
+            // Arrange
+            var left = new Matrix4(new Vector4(1, 0, 3, 4),
+                                   new Vector4(0, 1, 5, 55),
+                                   new Vector4(34, 56, 76, 29),
+                                   new Vector4(45, 66, 123, 4573));
+
+            var right = new Matrix4(new Vector4(1, 0, 3, 4),
+                                   new Vector4(0, 1, 5, 55),
+                                   new Vector4(34, 56, 76, 29),
+                                   new Vector4(45, 66, 123, 4573));
+
+            // Act
+            bool result = left.Equals(right);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Test]
+        public void Row0_Property_Test_Should_Be_Expected_Vector4()
+        {
+            // Arrange
+            var expected = new Vector4(1, 0, 4, 4);
+
+            // Act
+            var matrix = new Matrix4(expected,
+                                     new Vector4(0, 0, 0, 0),
+                                     new Vector4(0, 0, 0, 0),
+                                     new Vector4(0, 0, 0, 0));
+
+            // Assert
+            Assert.AreEqual(expected, matrix.Row0);
+        }
+
+        [Test]
+        public void Row1_Property_Test_Should_Be_Vector4()
+        {
+            // Arrange
+            var expected = new Vector4(1, 0, 4, 1);
+
+            // Act
+            var matrix = new Matrix4(new Vector4(0, 0, 0, 0),
+                                     expected,
+                                     new Vector4(0, 0, 0, 0),
+                                     new Vector4(0, 0, 0, 0));
+
+            // Assert
+            Assert.AreEqual(expected, matrix.Row1);
+        }
+
+        [Test]
+        public void Row2_Property_Test_Should_Be_Vector4()
+        {
+            // Arrange
+            var expected = new Vector4(1, 0, 4, 455);
+
+            // Act
+            var matrix = new Matrix4(new Vector4(0, 0, 0, 0),
+                                     new Vector4(0, 0, 0, 0),
+                                     expected,
+                                     new Vector4(0, 0, 0, 0));
+
+            // Assert
+            Assert.AreEqual(expected, matrix.Row2);
+        }
+
+        [Test]
+        public void Row3_Property_Test_Should_Be_Vector4()
+        {
+            // Arrange
+            var expected = new Vector4(1, 0, 4, 455);
+
+            // Act
+            var matrix = new Matrix4(new Vector4(0, 0, 0, 0),
+                                     new Vector4(0, 0, 0, 0),
+                                     new Vector4(0, 0, 0, 0),
+                                     expected);
+
+            // Assert
+            Assert.AreEqual(expected, matrix.Row3);
+        }
+    }
+}
---
+++ FinalEngine.Maths/Matrix4.cs
@@ -0,0 +1,61 @@
+﻿namespace FinalEngine.Maths
+{
+    using System;
+
+    public struct Matrix4 : IEquatable<Matrix4>
+    {
+        public Matrix4(Vector4 row0, Vector4 row1, Vector4 row2, Vector4 row3)
+        {
+            Row0 = row0;
+            Row1 = row1;
+            Row2 = row2;
+            Row3 = row3;
+        }
+
+        public Vector4 Row0 { get; set; }
+
+        public Vector4 Row1 { get; set; }
+
+        public Vector4 Row2 { get; set; }
+
+        public Vector4 Row3 { get; set; }
+
+        public static bool operator !=(Matrix4 left, Matrix4 right)
+        {
+            return !left.Equals(right);
+        }
+
+        public static bool operator ==(Matrix4 left, Matrix4 right)
+        {
+            return left.Equals(right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Matrix4))
+            {
+                return false;
+            }
+
+            return Equals((Matrix4)obj);
+        }
+
+        public bool Equals(Matrix4 other)
+        {
+            return Row0 == other.Row0 &&
+                   Row1 == other.Row1 &&
+                   Row2 == other.Row2 &&
+                   Row3 == other.Row3;
+        }
+
+        public override int GetHashCode()
+        {
+            return new { Row0, Row1, Row2, Row3 }.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"({ Row0 }\n{ Row1 }\n{ Row2 }\n{ Row3 })";
+        }
+    }
+}

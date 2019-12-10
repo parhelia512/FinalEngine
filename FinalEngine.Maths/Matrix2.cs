f5459c662f7503ed48788d2a5f08300c9ee1e956
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-10 23:07:44 +1100

    Update Matrix2 documentaiton

--- FinalEngine.Maths/Matrix2.cs
+++ FinalEngine.Maths/Matrix2.cs
@@ -8,10 +8,19 @@
     /// <seealso cref="System.IEquatable{FinalEngine.Maths.Matrix2}"/>
     public struct Matrix2 : IEquatable<Matrix2>
     {
+        /// <summary>
+        ///   Represents a <see cref="Matrix2"/> that is an identity matrix.
+        /// </summary>
         public static readonly Matrix2 Identity = new Matrix2(new Vector2(1, 0), new Vector2(0, 1));
 
+        /// <summary>
+        ///   Reperesents a <see cref="Matrix2"/> where each row is set to <see cref="Vector2.One"/>.
+        /// </summary>
         public static readonly Matrix2 One = new Matrix2(Vector2.One, Vector2.One);
 
+        /// <summary>
+        ///   Represents a <see cref="Matrix2"/> where each row is set to <see cref="Vector2.Zero"/>.
+        /// </summary>
         public static readonly Matrix2 Zero = new Matrix2(Vector2.Zero, Vector2.Zero);
 
         /// <summary>

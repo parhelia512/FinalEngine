c474e79ceeafc87f929c9d4f35bd27018ccce441
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-10 20:44:42 +1100

    Add shorthands for easier initialization of Vector2

--- FinalEngine.Maths/Vector2.cs
+++ FinalEngine.Maths/Vector2.cs
@@ -4,6 +4,22 @@
 
     public struct Vector2 : IEquatable<Vector2>
     {
+        public static Vector2 Down = new Vector2(0, -1);
+
+        public static Vector2 Left = new Vector2(-1, 0);
+
+        public static Vector2 NegativeInfinity = new Vector2(float.NegativeInfinity, float.NegativeInfinity);
+
+        public static Vector2 One = new Vector2(1, 1);
+
+        public static Vector2 PositiveInfinity = new Vector2(float.PositiveInfinity, float.PositiveInfinity);
+
+        public static Vector2 Right = new Vector2(1, 0);
+
+        public static Vector2 Up = new Vector2(0, 1);
+
+        public static Vector2 Zero = new Vector2(0, 0);
+
         public Vector2(float x, float y)
         {
             X = x;

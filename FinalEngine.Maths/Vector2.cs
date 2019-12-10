d739ad88a0be8c5500956b8e0e5a58c9c22ba44f
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-10 21:34:58 +1100

    Vector2 documented

--- FinalEngine.Maths/Vector2.cs
+++ FinalEngine.Maths/Vector2.cs
@@ -2,44 +2,126 @@
 {
     using System;
 
+    /// <summary>
+    ///   Represents a 2D vector with two single-precision floating point numbers.
+    /// </summary>
+    /// <seealso cref="System.IEquatable{FinalEngine.Maths.Vector2}"/>
     public struct Vector2 : IEquatable<Vector2>
     {
+        /// <summary>
+        ///   Represents a <see cref="Vector2"/> that point down (0, -1).
+        /// </summary>
         public static Vector2 Down = new Vector2(0, -1);
 
+        /// <summary>
+        ///   Represents a <see cref="Vector2"/> that points left (-1, 0).
+        /// </summary>
         public static Vector2 Left = new Vector2(-1, 0);
 
+        /// <summary>
+        ///   Represents a <see cref="Vector2"/> where each component is set to <see cref="float.NegativeInfinity"/>.
+        /// </summary>
         public static Vector2 NegativeInfinity = new Vector2(float.NegativeInfinity, float.NegativeInfinity);
 
+        /// <summary>
+        ///   Represents a <see cref="Vector2"/> where each component is set to one.
+        /// </summary>
         public static Vector2 One = new Vector2(1, 1);
 
+        /// <summary>
+        ///   Represents a <see cref="Vector2"/> where each component is set to <see cref="float.PositiveInfinity"/>.
+        /// </summary>
         public static Vector2 PositiveInfinity = new Vector2(float.PositiveInfinity, float.PositiveInfinity);
 
+        /// <summary>
+        ///   Represents a <see cref="Vector2"/> that points right (1, 0).
+        /// </summary>
         public static Vector2 Right = new Vector2(1, 0);
 
+        /// <summary>
+        ///   Represents a <see cref="Vector2"/> that points up (0, 1).
+        /// </summary>
         public static Vector2 Up = new Vector2(0, 1);
 
+        /// <summary>
+        ///   Represents a <see cref="Vector2"/> where each component is set to zero.
+        /// </summary>
         public static Vector2 Zero = new Vector2(0, 0);
 
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="Vector2"/> struct.
+        /// </summary>
+        /// <param name="x">
+        ///   The X component of this <see cref="Vector2"/>.
+        /// </param>
+        /// <param name="y">
+        ///   The y of this <see cref="Vector2"/>.
+        /// </param>
         public Vector2(float x, float y)
         {
             X = x;
             Y = y;
         }
 
+        /// <summary>
+        ///   Gets or sets a value that represents the X-coordinate of this <see cref="Vector2"/>.
+        /// </summary>
+        /// <value>
+        ///   The X-coordinate of this <see cref="Vector2"/>.
+        /// </value>
         public float X { get; set; }
 
+        /// <summary>
+        ///   Gets or sets a value that represents the Y-coordinate of this <see cref="Vector2"/>.
+        /// </summary>
+        /// <value>
+        ///   The Y-coordinate of this <see cref="Vector2"/>.
+        /// </value>
         public float Y { get; set; }
 
+        /// <summary>
+        ///   Implements the operator !=.
+        /// </summary>
+        /// <param name="left">
+        ///   The left operand.
+        /// </param>
+        /// <param name="right">
+        ///   The right operand.
+        /// </param>
+        /// <returns>
+        ///   <c>true</c> if the specified <paramref name="left"/> and <paramref name="right"/> parameters are not equal.
+        /// </returns>
         public static bool operator !=(Vector2 left, Vector2 right)
         {
             return !left.Equals(right);
         }
 
+        /// <summary>
+        ///   Implements the operator ==.
+        /// </summary>
+        /// <param name="left">
+        ///   The left operand.
+        /// </param>
+        /// <param name="right">
+        ///   The right operand.
+        /// </param>
+        /// <returns>
+        ///   <c>true</c> if the specified <paramref name="left"/> and <paramref name="right"/> parameters are equal.
+        /// </returns>
         public static bool operator ==(Vector2 left, Vector2 right)
         {
             return left.Equals(right);
         }
 
+        /// <summary>
+        ///   Determines whether the specified <see cref="System.Object"/>, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">
+        ///   The <see cref="System.Object"/> to compare with this instance.
+        /// </param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object"/> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
         public override bool Equals(object obj)
         {
             if (!(obj is Vector2))
@@ -50,17 +132,38 @@
             return Equals((Vector2)obj);
         }
 
+        /// <summary>
+        ///   Determines whether the specified <paramref name="other"/> parameter, is equal to this <see cref="Vector2"/>.
+        /// </summary>
+        /// <param name="other">
+        ///   The <see cref="Vector2"/> to compare with this <see cref="Vector2"/>.
+        /// </param>
+        /// <returns>
+        ///   <c>true</c> if the specified <paramref name="other"/> parameter is equal to this <see cref="Vector2"/>; otherwise, <c>false</c>.
+        /// </returns>
         public bool Equals(Vector2 other)
         {
             return X == other.X &&
                    Y == other.Y;
         }
 
+        /// <summary>
+        ///   Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        ///   A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.
+        /// </returns>
         public override int GetHashCode()
         {
             return new { X, Y }.GetHashCode();
         }
 
+        /// <summary>
+        ///   Converts to string.
+        /// </summary>
+        /// <returns>
+        ///   A <see cref="System.String"/> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
             return $"({ X }, { Y })";

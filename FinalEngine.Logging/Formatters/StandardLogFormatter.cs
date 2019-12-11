439d441f36f447e5af63e9ed2cc535b385f2e36a
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-11 22:44:53 +1100

    Create StandardLogFormatter and unit tests

---
+++ FinalEngine.Logging/Formatters/StandardLogFormatter.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalEngine.Logging.Formatters
+{
+    public sealed class StandardLogFormatter : ILogFormatter
+    {
+        public string GetFormattedLog(LogType type, string message)
+        {
+            return $"[{ type.ToString().ToUpper() }] { message }";
+        }
+    }
+}

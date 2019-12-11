c555fe6c0467517c8ef10d7f7ae14248830cc4e6
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-11 21:50:47 +1100

    Proof of concept that the log handler and formatter idea is working, time to make proper implementations

---
+++ FinalEngine.Launcher.Desktop/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FinalEngine.Launcher.Desktop
+{
+    using System;
+    using FinalEngine.Logging;
+    using FinalEngine.Logging.Formatters;
+    using FinalEngine.Logging.Handlers;
+
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var formatter = new StandardLogFormatter();
+            var logger = new TextWriterLogHandler(formatter, Console.Out);
+
+            logger.Log(LogType.Information, "Test logging");
+            logger.Log(LogType.Debug, "Test debugging");
+            logger.Log(LogType.Error, "Test error");
+            logger.Log(LogType.Warning, "Test warning");
+        }
+    }
+}

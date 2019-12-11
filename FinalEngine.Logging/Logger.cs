9f0d6975994c02f0613ecf1891b4c927fd4e5195
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-11 23:19:03 +1100

    Create Logger and unit tests

---
+++ FinalEngine.Logging/Logger.cs
@@ -0,0 +1,32 @@
+﻿namespace FinalEngine.Logging
+{
+    using System.Collections.Generic;
+
+    public sealed class Logger : ILogger
+    {
+        private static ILogger instance;
+
+        public Logger()
+        {
+            Handlers = new List<ILogHandler>();
+        }
+
+        public static ILogger Instance
+        {
+            get
+            {
+                return instance ?? (instance = new Logger());
+            }
+        }
+
+        public ICollection<ILogHandler> Handlers { get; }
+
+        public void Log(LogType type, string message)
+        {
+            foreach (ILogHandler handler in Handlers)
+            {
+                handler.Log(type, message);
+            }
+        }
+    }
+}

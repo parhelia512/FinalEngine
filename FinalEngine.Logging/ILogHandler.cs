5f19891ccf5b536245e7df60321e068f9e44b05f
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-12 00:30:07 +1100

    Update documentation some more; the introduction of example code has ruined our chances of using CodeMaid to format XML documents :(

--- FinalEngine.Logging/ILogHandler.cs
+++ FinalEngine.Logging/ILogHandler.cs
@@ -4,7 +4,7 @@
     ///   Defines an interface that represents a log handler.
     /// </summary>
     /// <remarks>
-    ///   <para>An <see cref="ILogHandler"/> provides a way for users to extend the API and allow information to be logged to other destinations (such as a database or the console). An <see cref="ILogHandler"/> provides a single method; <see cref="ILogHandler.Log(LogType, string)"/> that user must implement.</para>
+    ///   <para>An <see cref="ILogHandler"/> provides a way for users to extend the API and allow information to be logged to other destinations (such as a database or the console). An <see cref="ILogHandler"/> provides a single method; <see cref="Log(LogType, string)"/> that user must implement.</para>
     ///   <para>If a user wants to implement the <see cref="ILogHandler"/> interface for the sole purpose of creating a log handler (and not to log to multiple log handlers) he should instead inherit from <see cref="LogHandler"/></para>
     /// </remarks>
     public interface ILogHandler

a0df83e378abe053a8bf507260af83f555ceb85e
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-11 16:14:09 +1100

    Create IDisplay

---
+++ FinalEngine.Platform/IDisplay.cs
@@ -0,0 +1,26 @@
+﻿namespace FinalEngine.Platform
+{
+    using System;
+    using System.Drawing;
+
+    public interface IDisplay : IDisposable
+    {
+        Rectangle ClientRectangle { get; }
+
+        Size ClientSize { get; }
+
+        bool Focused { get; }
+
+        bool IsClosing { get; }
+
+        Point Location { get; set; }
+
+        Size Size { get; set; }
+
+        string Title { get; set; }
+
+        bool Visible { get; set; }
+
+        void Close();
+    }
+}

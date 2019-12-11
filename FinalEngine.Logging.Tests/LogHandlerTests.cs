573d021595e0e3774d71f14175c80d3c84a4d7c5
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-11 22:21:00 +1100

    Create LogHandler and tested the best I can

---
+++ FinalEngine.Logging.Tests/LogHandlerTests.cs
@@ -0,0 +1,15 @@
+﻿namespace FinalEngine.Logging.Tests
+{
+    using System;
+    using Moq;
+    using NUnit.Framework;
+
+    public sealed class LogHandlerTests
+    {
+        [Test]
+        public void Constructor_Test_Should_Throw_ArgumentNulLException_When_LogFormatter_Is_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Mock<LogHandler>(null));
+        }
+    }
+}
---
+++ FinalEngine.Logging/LogHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace FinalEngine.Logging
+{
+    using System;
+
+    public abstract class LogHandler : ILogHandler
+    {
+        protected LogHandler(ILogFormatter formatter)
+        {
+            Formatter = formatter ?? throw new ArgumentNullException(nameof(formatter), $"The specified { nameof(formatter) } parameter is null.");
+        }
+
+        protected ILogFormatter Formatter { get; }
+
+        public abstract void Log(LogType type, string message);
+    }
+}

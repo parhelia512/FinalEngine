97a933f0c5cddc2eb771872cf98e131960f2cdb1
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-12 00:35:07 +1100

    Update documentation

--- FinalEngine.Logging/ILogger.cs
+++ FinalEngine.Logging/ILogger.cs
@@ -5,6 +5,9 @@
     /// <summary>
     ///   Defines an interface that represents a central logger.
     /// </summary>
+    /// <remarks>
+    ///   An <see cref="ILogger"/> provides a place for users to add, remove and access <see cref="ILogHandler"/> s. An <see cref="ILogger"/> also implements the <see cref="ILogHandler"/> interface to allow users to create separate loggers and join them together.
+    /// </remarks>
     /// <seealso cref="FinalEngine.Logging.ILogHandler"/>
     public interface ILogger : ILogHandler
     {

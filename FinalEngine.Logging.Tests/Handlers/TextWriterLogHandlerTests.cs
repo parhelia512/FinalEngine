10919e98c55c16a5b2b7c10c7c3416624d10cf2f
Mathew O'Dwyer <50978201+mtosoftware@users.noreply.github.com>
2019-12-12 14:03:02 +1100

    Added unit tests

--- FinalEngine.Logging.Tests/Handlers/TextWriterLogHandlerTests.cs
+++ FinalEngine.Logging.Tests/Handlers/TextWriterLogHandlerTests.cs
@@ -55,5 +55,31 @@
             mockTextWriter.Verify(t => t.WriteLine(It.IsAny<string>()), Times.Once);
             mockLogFormatter.Verify(f => f.GetFormattedLog(It.IsAny<LogType>(), It.IsAny<string>()), Times.Once);
         }
+        
+        [Test]
+        public void Log_Test_Should_Throw_ArgumentNullException_When_Message_Is_Null()
+        {
+            // Arrange
+            var mockLogFormatter = new Mock<ILogFormatter>();
+            var mockTextWriter = new Mock<TextWriter>();
+
+            var logHandler = new TextWriterLogHandler(mockLogFormatter.Object, mockTextWriter.Object);
+            
+            // Act and assert
+            Assert.Throws<ArgumentNullException>(() => logHandler.Log(It.IsAny<LogType>(), null));
+        }
+        
+        [Test]
+        public void Log_Test_Should_Throw_ArgumentNullException_When_Message_Is_Empty()
+        {
+            // Arrange
+            var mockLogFormatter = new Mock<ILogFormatter>();
+            var mockTextWriter = new Mock<TextWriter>();
+
+            var logHandler = new TextWriterLogHandler(mockLogFormatter.Object, mockTextWriter.Object);
+            
+            // Act and assert
+            Assert.Throws<ArgumentNullException>(() => logHandler.Log(It.IsAny<LogType>(), string.Empty));
+        }
     }
 }

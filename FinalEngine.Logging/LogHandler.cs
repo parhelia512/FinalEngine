ce171d3169716a635084601ba6b9af39160f50d0
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 22:02:17 +1100

    More documentation

--- FinalEngine.Logging/LogHandler.cs
+++ FinalEngine.Logging/LogHandler.cs
@@ -2,15 +2,46 @@
 {
     using System;
 
+    /// <summary>
+    ///   Provides an abstraction representation of an <see cref="ILogHandler"/> that provides the ability to assure correct formatting of messages.
+    /// </summary>
+    /// <seealso cref="FinalEngine.Logging.ILogHandler"/>
     public abstract class LogHandler : ILogHandler
     {
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="LogHandler"/> class.
+        /// </summary>
+        /// <param name="formatter">
+        ///   Specifies a <see cref="ILogFormatter"/> that represents the formatter that will format a message before it is logged.
+        /// </param>
+        /// <exception cref="System.ArgumentNullException">
+        ///   The specified <paramref name="formatter"/> parameter is null.
+        /// </exception>
         protected LogHandler(ILogFormatter formatter)
         {
             Formatter = formatter ?? throw new ArgumentNullException(nameof(formatter), $"The specified { nameof(formatter) } parameter is null.");
         }
 
+        /// <summary>
+        ///   Gets a <see cref="ILogFormatter"/> that represents the formatter of this <see cref="LogHandler"/>.
+        /// </summary>
+        /// <value>
+        ///   The formatter of this <see cref="LogHandler"/>.
+        /// </value>
         protected ILogFormatter Formatter { get; }
 
+        /// <summary>
+        ///   Logs the specified <paramref name="message"/> of the specified <paramref name="type"/>.
+        /// </summary>
+        /// <param name="type">
+        ///   Specifies a <see cref="LogType"/> that represents the type of message to be logged.
+        /// </param>
+        /// <param name="message">
+        ///   Specifies a <see cref="string"/> that represents the message to be logged.
+        /// </param>
+        /// <remarks>
+        ///   When implementing this method in a derived class, you should always remember to call the <see cref="ILogFormatter.GetFormattedLog(LogType, string)"/> method of the <see cref="Formatter"/> property to allow the message to be formatted before it is logged.
+        /// </remarks>
         public abstract void Log(LogType type, string message);
     }
 }

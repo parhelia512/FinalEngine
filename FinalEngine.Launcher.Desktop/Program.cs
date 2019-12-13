5c5bf8c5794193ad4960315db17164ae1d2e1edb
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 19:54:24 +1100

    Remove FinalEngine.Platform.Windows project

--- FinalEngine.Launcher.Desktop/Program.cs
+++ FinalEngine.Launcher.Desktop/Program.cs
@@ -1,11 +1,9 @@
 ﻿namespace FinalEngine.Launcher.Desktop
 {
     using System;
-    using System.Runtime.InteropServices;
     using FinalEngine.Logging;
     using FinalEngine.Logging.Formatters;
     using FinalEngine.Logging.Handlers;
-    using FinalEngine.Platform.Windows;
 
     /// <summary>
     ///   The main program.
@@ -20,26 +18,6 @@
             Logger.Instance.Handlers.Add(new TextWriterLogHandler(new StandardLogFormatter(), Console.Out));
 
             Logger.Instance.Log(LogType.Information, "Final Engine is starting...");
-            Logger.Instance.Log(LogType.Information, "OS: " + RuntimeInformation.OSDescription);
-            Logger.Instance.Log(LogType.Information, "NET Version: " + RuntimeInformation.FrameworkDescription);
-
-            Logger.Instance.Log(LogType.Information, "Creating display...");
-
-            var display = new WinFormDisplay(1024, 768, "Final Engine")
-            {
-                Visible = true
-            };
-
-            var eventsProcessor = new WinFormEventsProcessor();
-
-            Logger.Instance.Log(LogType.Information, "Entering main loop...");
-
-            while (!display.IsClosing)
-            {
-                eventsProcessor.ProcessEvents();
-            }
-
-            display.Dispose();
         }
     }
 }

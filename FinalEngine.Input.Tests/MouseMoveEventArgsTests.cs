7c9b87d24f9108fc253d8bcf6b0590b6981901bf
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 17:31:50 +1100

    Add unit tests for MouseMoveEventArgs

---
+++ FinalEngine.Input.Tests/MouseMoveEventArgsTests.cs
@@ -0,0 +1,24 @@
+﻿namespace FinalEngine.Input.Tests
+{
+    using FinalEngine.Drawing;
+    using FinalEngine.Input.Events;
+    using NUnit.Framework;
+
+    public sealed class MouseMoveEventArgsTests
+    {
+        [Test]
+        public void Location_Property_Test()
+        {
+            // Arrange
+            var expected = new PointF(34, 21);
+
+            var mouseMoveEventArgs = new MouseMoveEventArgs(expected);
+
+            // Act
+            PointF actual = mouseMoveEventArgs.Location;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

39378c4acf3707804851cc8fc75b43251569925c
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 17:27:08 +1100

    More unit testing

--- FinalEngine.Input.Tests/KeyEventArgsTests.cs
+++ FinalEngine.Input.Tests/KeyEventArgsTests.cs
@@ -46,5 +46,64 @@
             // Assert
             Assert.AreEqual(Expected, actual);
         }
+
+        [Test]
+        public void KeyModifiers_Property_Test_Should_Be_Control_And_Alt()
+        {
+            // Arrange
+            const KeyModifier Expected = KeyModifier.Control | KeyModifier.Alt;
+
+            var keyEventArgs = new KeyEventArgs(It.IsAny<Key>(),
+                                                It.IsAny<LockableKeyState>(),
+                                                It.IsAny<LockableKeyState>(),
+                                                It.IsAny<LockableKeyState>(),
+                                                Expected);
+
+            // Act
+            KeyModifier actual = keyEventArgs.KeyModifiers;
+
+            // Assert
+            Assert.AreEqual(Expected, actual);
+        }
+
+        [Test]
+        public void NumLockState_Property_Test()
+        {
+            // Arrange
+            Array values = Enum.GetValues(typeof(LockableKeyState));
+            var Expected = (LockableKeyState)values.GetValue(new Random().Next(values.Length));
+
+            var keyEventArgs = new KeyEventArgs(It.IsAny<Key>(),
+                                                It.IsAny<LockableKeyState>(),
+                                                Expected,
+                                                It.IsAny<LockableKeyState>(),
+                                                It.IsAny<KeyModifier>());
+
+            // Act
+            LockableKeyState actual = keyEventArgs.NumLockState;
+
+            // Assert
+            Assert.AreEqual(Expected, actual);
+        }
+
+        [Test]
+        public void ScrollLockState_Property_Test()
+        {
+            // Arrange
+            Array values = Enum.GetValues(typeof(LockableKeyState));
+            var Expected = (LockableKeyState)values.GetValue(new Random().Next(values.Length));
+
+            var keyEventArgs = new KeyEventArgs(It.IsAny<Key>(),
+                                                It.IsAny<LockableKeyState>(),
+                                                It.IsAny<LockableKeyState>(),
+                                                Expected,
+                                                It.IsAny<KeyModifier>());
+
+            // Act
+            LockableKeyState actual = keyEventArgs.ScrollLockState;
+
+            // Assert
+            Assert.AreEqual(Expected, actual);
+        }
     }
 }

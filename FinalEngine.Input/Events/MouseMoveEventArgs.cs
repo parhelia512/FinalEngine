244a6cfefd485d6708abf05c037aa37e60fe4792
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 16:39:16 +1100

    Create MouseMoveEventArgs

---
+++ FinalEngine.Input/Events/MouseMoveEventArgs.cs
@@ -0,0 +1,19 @@
+﻿namespace FinalEngine.Input.Events
+{
+    using System;
+    using FinalEngine.Drawing;
+
+    public sealed class MouseMoveEventArgs : EventArgs
+    {
+        public MouseMoveEventArgs(PointF location, PointF delta)
+        {
+            Location = location;
+            Delta = delta;
+        }
+
+        // The change in position produced by this event
+        public PointF Delta { get; }
+
+        public PointF Location { get; }
+    }
+}

ffb2c810b288547e53dcd6f6f2e5baeea6164b54
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 17:05:09 +1100

    Update documentation

--- FinalEngine.Input/MouseButton.cs
+++ FinalEngine.Input/MouseButton.cs
@@ -1,7 +1,13 @@
 ﻿namespace FinalEngine.Input
 {
+    /// <summary>
+    ///   Enumerates the common available buttons on a standard mouse.
+    /// </summary>
     public enum MouseButton
     {
+        /// <summary>
+        ///   The specified button is unknown.
+        /// </summary>
         Unknown
     }
 }

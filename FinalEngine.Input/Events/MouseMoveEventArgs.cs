7f01842174574722b180cb5eb40f19c328160c81
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 16:58:21 +1100

    Update documentation for MouseMoveEventArgs

--- FinalEngine.Input/Events/MouseMoveEventArgs.cs
+++ FinalEngine.Input/Events/MouseMoveEventArgs.cs
@@ -2,18 +2,31 @@
 {
     using System;
     using FinalEngine.Drawing;
+    using FinalEngine.Input.Devices;
 
+    /// <summary>
+    ///   Provides event data for the <see cref="IMouseDevice.PositionChanged"/> event.
+    /// </summary>
+    /// <seealso cref="System.EventArgs"/>
     public sealed class MouseMoveEventArgs : EventArgs
     {
-        public MouseMoveEventArgs(PointF location, PointF delta)
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="MouseMoveEventArgs"/> class.
+        /// </summary>
+        /// <param name="location">
+        ///   Specifies the location of the cursor coordinates (in pixels) relative to the upper-left corner of a display.
+        /// </param>
+        public MouseMoveEventArgs(PointF location)
         {
             Location = location;
-            Delta = delta;
         }
 
-        // The change in position produced by this event
-        public PointF Delta { get; }
-
+        /// <summary>
+        ///   Gets a <see cref="PointF"/> that represents the location (in pixels) of the cursor, relative to the upper-left corner of a display.
+        /// </summary>
+        /// <value>
+        ///   The location (in pixels) of the cursor, relative to the upper-left corner of a display.
+        /// </value>
         public PointF Location { get; }
     }
 }

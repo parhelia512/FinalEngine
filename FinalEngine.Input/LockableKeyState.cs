4780434968fc4f65e75068f36e4bdc6fa8b0a9bb
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 17:02:27 +1100

    Update documentaiton

--- FinalEngine.Input/LockableKeyState.cs
+++ FinalEngine.Input/LockableKeyState.cs
@@ -1,9 +1,18 @@
 ﻿namespace FinalEngine.Input
 {
+    /// <summary>
+    ///   Enumerates the available states a lockable key can be in.
+    /// </summary>
     public enum LockableKeyState
     {
+        /// <summary>
+        ///   Specifies the state of the key is locked.
+        /// </summary>
         Locked,
 
+        /// <summary>
+        ///   Specifies the state of the key is unlocked.
+        /// </summary>
         Unlocked
     }
 }

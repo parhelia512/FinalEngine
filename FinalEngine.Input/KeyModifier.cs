9e8ef5a06b185c19ca30ddbbaca2bb1ba268e462
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 17:04:01 +1100

    Update documentation

--- FinalEngine.Input/KeyModifier.cs
+++ FinalEngine.Input/KeyModifier.cs
@@ -2,15 +2,33 @@
 {
     using System;
 
+    /// <summary>
+    ///   Enumerates the available modifier keys on a standard keyboard.
+    /// </summary>
     [Flags]
     public enum KeyModifier
     {
+        /// <summary>
+        ///   Specifies the shfit key.
+        /// </summary>
         Shift,
 
+        /// <summary>
+        ///   Specifies the control key.
+        /// </summary>
         Control,
 
+        /// <summary>
+        ///   Specifies the ALT key.
+        /// </summary>
         Alt,
 
+        /// <summary>
+        ///   Specifies the OS-specific key.
+        /// </summary>
+        /// <remarks>
+        ///   On Windows, the modifier should be a Windows key, whereas on Macintosh it would be the Command key.
+        /// </remarks>
         OS
     }
 }

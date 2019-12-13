1b0f399350ac89cbb0bd1ec203cf3918bcc94170
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 16:43:20 +1100

    Create IMouseDevice

---
+++ FinalEngine.Input/Devices/IMouseDevice.cs
@@ -0,0 +1,16 @@
+﻿namespace FinalEngine.Input.Devices
+{
+    using System;
+    using FinalEngine.Input.Events;
+
+    public interface IMouseDevice
+    {
+        event EventHandler<MouseButtonEventArgs> ButtonDown;
+
+        event EventHandler<MouseButtonEventArgs> ButtonUp;
+
+        event EventHandler<MouseMoveEventArgs> MouseMove;
+
+        event EventHandler<MouseMoveEventArgs> MouseWheel;
+    }
+}

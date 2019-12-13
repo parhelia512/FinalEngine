1d82eaea942c08124baab1c7b1d56bb1df2d6542
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 16:42:05 +1100

    Create MouseWheelEventArgs

---
+++ FinalEngine.Input/Events/MouseWheelEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace FinalEngine.Input.Events
+{
+    using System;
+
+    public sealed class MouseWheelEventArgs : EventArgs
+    {
+        public MouseWheelEventArgs(float delta)
+        {
+            Delta = delta;
+        }
+
+        public float Delta { get; }
+    }
+}

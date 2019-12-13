9effa04439e639734f70e2298345f17b74132be7
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 17:24:23 +1100

    Slowly adding unit tests

---
+++ FinalEngine.Input.Tests/KeyEventArgsTests.cs
@@ -0,0 +1,50 @@
+﻿namespace FinalEngine.Input.Tests
+{
+    using System;
+    using FinalEngine.Input.Events;
+    using Moq;
+    using NUnit.Framework;
+
+    public sealed class KeyEventArgsTests
+    {
+        [Test]
+        public void CapsLockState_Property_Test()
+        {
+            // Arrange
+            Array values = Enum.GetValues(typeof(LockableKeyState));
+            var Expected = (LockableKeyState)values.GetValue(new Random().Next(values.Length));
+
+            var keyEventArgs = new KeyEventArgs(It.IsAny<Key>(),
+                                                Expected,
+                                                It.IsAny<LockableKeyState>(),
+                                                It.IsAny<LockableKeyState>(),
+                                                It.IsAny<KeyModifier>());
+
+            // Act
+            LockableKeyState actual = keyEventArgs.CapsLockState;
+
+            // Assert
+            Assert.AreEqual(Expected, actual);
+        }
+
+        [Test]
+        public void Key_Property_Test()
+        {
+            // Arrange
+            Array values = Enum.GetValues(typeof(Key));
+            var Expected = (Key)values.GetValue(new Random().Next(values.Length));
+
+            var keyEventArgs = new KeyEventArgs(Expected,
+                                                It.IsAny<LockableKeyState>(),
+                                                It.IsAny<LockableKeyState>(),
+                                                It.IsAny<LockableKeyState>(),
+                                                It.IsAny<KeyModifier>());
+
+            // Act
+            Key actual = keyEventArgs.Key;
+
+            // Assert
+            Assert.AreEqual(Expected, actual);
+        }
+    }
+}

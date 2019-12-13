56579e2cc9ff944802f75ba37011f0fdbbe112d1
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 17:29:40 +1100

    Unit tests done for MouseButtonEventArgs

---
+++ FinalEngine.Input.Tests/MouseButtonEventArgsTests.cs
@@ -0,0 +1,25 @@
+﻿namespace FinalEngine.Input.Tests
+{
+    using System;
+    using FinalEngine.Input.Events;
+    using NUnit.Framework;
+
+    public sealed class MouseButtonEventArgsTests
+    {
+        [Test]
+        public void MouseButton_Property_Test()
+        {
+            // Arrange
+            Array values = Enum.GetValues(typeof(MouseButton));
+            var Expected = (MouseButton)values.GetValue(new Random().Next(values.Length));
+
+            var mouseButtonEventArgs = new MouseButtonEventArgs(Expected);
+
+            // Act
+            MouseButton actual = mouseButtonEventArgs.Button;
+
+            // Assert
+            Assert.AreEqual(Expected, actual);
+        }
+    }
+}

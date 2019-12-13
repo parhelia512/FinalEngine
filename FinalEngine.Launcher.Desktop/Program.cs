b591019edf0582d3fdd0b33c66257cf3a7c7bc51
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 23:43:50 +1100

    Create FinalEngine.Launcher.Desktop project and update StyleCop settings

---
+++ FinalEngine.Launcher.Desktop/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FinalEngine.Launcher.Desktop
+{
+    using System;
+
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

768c8063d5dd8e5f92b8d2b883159198616f76db
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 23:50:12 +1100

    Hello, DIsplay! [.NET Core]

--- FinalEngine.Launcher.Desktop/Program.cs
+++ FinalEngine.Launcher.Desktop/Program.cs
@@ -1,12 +1,36 @@
 ﻿namespace FinalEngine.Launcher.Desktop
 {
     using System;
+    using FinalEngine.Logging;
+    using FinalEngine.Logging.Formatters;
+    using FinalEngine.Logging.Handlers;
+    using FinalEngine.Platform.Desktop;
 
     internal static class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            ILogger logger = Logger.Instance;
+
+            // Log to the console, maybe we should make a ConsoleLogHandler, that'll color code the console?
+            logger.Handlers.Add(new TextWriterLogHandler(new StandardLogFormatter(), Console.Out));
+            logger.Log(LogType.Information, "Final Engine is starting...");
+
+            var display = new OpenTKDisplay(1024, 768, "Final Engine")
+            {
+                Visible = true
+            };
+
+            logger.Log(LogType.Information, "Entering game loop...");
+
+            while (!display.IsClosing)
+            {
+                display.ProcessEvents();
+            }
+
+            logger.Log(LogType.Information, "Disposing of resources...");
+
+            display.Dispose();
         }
     }
 }

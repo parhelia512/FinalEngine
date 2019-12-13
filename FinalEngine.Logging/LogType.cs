b27cbf91707ccb19aeb0d5f47036aa2e26ae9db7
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 22:08:42 +1100

    More documentation stuff uggghhhh

--- FinalEngine.Logging/LogType.cs
+++ FinalEngine.Logging/LogType.cs
@@ -1,13 +1,28 @@
 ﻿namespace FinalEngine.Logging
 {
+    /// <summary>
+    ///   Enumerates the available types of messages that can be logged.
+    /// </summary>
     public enum LogType
     {
+        /// <summary>
+        ///   Specifies the log message is meant to be treated as an error.
+        /// </summary>
         Error,
 
+        /// <summary>
+        ///   Specifies the log message is meant to be treated as a warning.
+        /// </summary>
         Warning,
 
+        /// <summary>
+        ///   Specifies the log message is meant to be treated as informative.
+        /// </summary>
         Information,
 
+        /// <summary>
+        ///   Specifies the log message is meant to be treated as debug information.
+        /// </summary>
         Debug
     }
 }

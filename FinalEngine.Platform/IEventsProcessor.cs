e82e1f8237a0de979a82d435000db3ee883db8cf
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 22:32:47 +1100

    One more project to document

--- FinalEngine.Platform/IEventsProcessor.cs
+++ FinalEngine.Platform/IEventsProcessor.cs
@@ -1,7 +1,13 @@
 ﻿namespace FinalEngine.Platform
 {
+    /// <summary>
+    ///   Defines an interface that provides a method for processing OS-specific events in a message queue.
+    /// </summary>
     public interface IEventsProcessor
     {
+        /// <summary>
+        ///   Processes the events that are currently in the message queue.
+        /// </summary>
         void ProcessEvents();
     }
 }

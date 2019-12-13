e104978988e4f31a8416099d238f84146278f8d9
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 15:59:59 +1100

    Hello, Display on Windows!

---
+++ FinalEngine.Launcher.Desktop/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace FinalEngine.Launcher.Desktop
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using FinalEngine.Logging;
+    using FinalEngine.Logging.Formatters;
+    using FinalEngine.Logging.Handlers;
+    using FinalEngine.Platform.Windows;
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Logger.Instance.Handlers.Add(new TextWriterLogHandler(new StandardLogFormatter(), Console.Out));
+
+            Logger.Instance.Log(LogType.Information, "Final Engine is starting...");
+            Logger.Instance.Log(LogType.Information, "OS: " + RuntimeInformation.OSDescription);
+            Logger.Instance.Log(LogType.Information, "NET Version: " + RuntimeInformation.FrameworkDescription);
+
+            Logger.Instance.Log(LogType.Information, "Creating display...");
+
+            var display = new WinFormDisplay(1024, 768, "Final Engine")
+            {
+                Visible = true
+            };
+
+            var eventsProcessor = new WinFormEventsProcessor();
+
+            Logger.Instance.Log(LogType.Information, "Entering main loop...");
+
+            while (!display.IsClosing)
+            {
+                eventsProcessor.ProcessEvents();
+            }
+
+            display.Dispose();
+        }
+    }
+}
---
+++ FinalEngine.Platform.Windows/WinFormEventsProcessor.cs
@@ -0,0 +1,13 @@
+﻿namespace FinalEngine.Platform.Windows
+{
+    using System.Windows.Forms;
+
+    public sealed class WinFormEventsProcessor : IEventsProcessor
+    {
+        public void ProcessEvents()
+        {
+            // TODO: Look into processing events similar to how SharpDX did it, but for now this is good.
+            Application.DoEvents();
+        }
+    }
+}

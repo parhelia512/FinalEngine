90bc2eb51fe03515376c850fe7042e542b772a1f
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-13 16:06:11 +1100

    Update documentation

--- FinalEngine.Platform.Windows/WinFormEventsProcessor.cs
+++ FinalEngine.Platform.Windows/WinFormEventsProcessor.cs
@@ -2,8 +2,15 @@
 {
     using System.Windows.Forms;
 
+    /// <summary>
+    ///   Provides an implementation of an <see cref="IEventsProcessor"/> that processes events on the Windows operating system.
+    /// </summary>
+    /// <seealso cref="FinalEngine.Platform.IEventsProcessor"/>
     public sealed class WinFormEventsProcessor : IEventsProcessor
     {
+        /// <summary>
+        ///   Processes all Windows events that are currently in the message queue.
+        /// </summary>
         public void ProcessEvents()
         {
             // TODO: Look into processing events similar to how SharpDX did it, but for now this is good.

861456aa3741b1ebb98ccd5034d2a67359a3ee4b
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-18 23:54:51 +1100

    Add RSSetViewport method to D3D11DeviceContextInvoker

---
+++ FinalEngine.Rendering.Direct3D11/Invoking/D3D11DeviceContextInvoker.cs
@@ -0,0 +1,20 @@
+﻿namespace FinalEngine.Rendering.Direct3D11.Invoking
+{
+    using System;
+    using Vortice.Direct3D11;
+
+    public sealed class D3D11DeviceContextInvoker : ID3D11DeviceContextInvoker
+    {
+        private readonly ID3D11DeviceContext deviceContext;
+
+        public D3D11DeviceContextInvoker(ID3D11DeviceContext deviceContext)
+        {
+            this.deviceContext = deviceContext ?? throw new ArgumentNullException(nameof(deviceContext), $"The specified { nameof(deviceContext) } parameter is null.");
+        }
+
+        public void RSSetViewport(float x, float y, float width, float height)
+        {
+            deviceContext.RSSetViewport(x, y, width, height);
+        }
+    }
+}

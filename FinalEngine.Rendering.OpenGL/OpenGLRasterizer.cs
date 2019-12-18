1d0ed979547ec161634bd617dbdf54f2988cb7ad
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-18 21:36:57 +1100

    OpenGLRasterizer now takes in an invoker as a parameter

--- FinalEngine.Rendering.OpenGL/OpenGLRasterizer.cs
+++ FinalEngine.Rendering.OpenGL/OpenGLRasterizer.cs
@@ -1,7 +1,17 @@
 ﻿namespace FinalEngine.Rendering.OpenGL
 {
+    using System;
+    using FinalEngine.Rendering.OpenGL.Invoking;
+
     public sealed class OpenGLRasterizer : IRasterizer
     {
+        private readonly IOpenGLInvoker invoker;
+
+        public OpenGLRasterizer(IOpenGLInvoker invoker)
+        {
+            this.invoker = invoker ?? throw new ArgumentNullException(nameof(invoker), $"The specified { nameof(invoker) } parameter is null.");
+        }
+
         public void SetViewport(int x, int y, int width, int height)
         {
             throw new System.NotImplementedException();

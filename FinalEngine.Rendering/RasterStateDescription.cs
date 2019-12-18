46716924e0319b6f6df780f5fb637a1664106b49
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-18 22:13:33 +1100

    Create basic RasterStateDescription

--- FinalEngine.Rendering/RasterStateDescription.cs
+++ FinalEngine.Rendering/RasterStateDescription.cs
@@ -1,6 +1,36 @@
 ﻿namespace FinalEngine.Rendering
 {
+    public enum FaceCullMode
+    {
+        None,
+
+        Front,
+
+        Back
+    }
+
+    public enum RasterMode
+    {
+        Solid,
+
+        Wireframe
+    }
+
+    public enum WindingDirection
+    {
+        Clockwise,
+
+        CounterClockwise
+    }
+
     public struct RasterStateDescription
     {
+        public bool CullEnabled { get; set; }
+
+        public FaceCullMode FaceCullMode { get; set; }
+
+        public RasterMode FillMode { get; set; }
+
+        public WindingDirection WindingDirection { get; set; }
     }
 }

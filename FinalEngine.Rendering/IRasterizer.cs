bbdf0df1c71d72e19d211fdd81b6251ee901dcb5
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-18 22:01:51 +1100

    Updating documentation for IRasterizer

--- FinalEngine.Rendering/IRasterizer.cs
+++ FinalEngine.Rendering/IRasterizer.cs
@@ -1,7 +1,25 @@
 ﻿namespace FinalEngine.Rendering
 {
+    /// <summary>
+    ///   Defines an interface that represents the rasterization stage of a rendering pipeline.
+    /// </summary>
     public interface IRasterizer
     {
+        /// <summary>
+        ///   Binds the viewport, specified by the <paramref name="x"/>, <paramref name="y"/>, <paramref name="width"/> and <paramref name="height"/> parameters to the rasterization stage of the rendering pipeline.
+        /// </summary>
+        /// <param name="x">
+        ///   Specifies a <see cref="Int32"/> that represents the X-coordinate of the viewport, in pixels.
+        /// </param>
+        /// <param name="y">
+        ///   Specifies a <see cref="Int32"/> that represents the Y-coordinate of the viewport, in pixels.
+        /// </param>
+        /// <param name="width">
+        ///   Specifies a <see cref="Int32"/> that represents the width of the viewport, in pixels.
+        /// </param>
+        /// <param name="height">
+        ///   Specifies a <see cref="Int32"/> that represents the height of the viewport, in pixels.
+        /// </param>
         void SetViewport(int x, int y, int width, int height);
     }
 }

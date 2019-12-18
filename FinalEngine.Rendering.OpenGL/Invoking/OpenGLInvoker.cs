99e5a80412b65a99c8f2d6849dbbaf9a68a98ee6
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-18 21:35:32 +1100

    Create OpenGLInvoker

---
+++ FinalEngine.Rendering.OpenGL/Invoking/OpenGLInvoker.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalEngine.Rendering.OpenGL.Invoking
+{
+    public sealed class OpenGLInvoker : IOpenGLInvoker
+    {
+        public void SetViewport(int x, int y, int width, int height)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

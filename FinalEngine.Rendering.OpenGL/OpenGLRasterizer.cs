92f43e47f9ac90d814870cba4dbd4016608d40d3
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-18 21:34:18 +1100

    Create OpenGLRasterizer

---
+++ FinalEngine.Rendering.OpenGL/OpenGLRasterizer.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalEngine.Rendering.OpenGL
+{
+    public sealed class OpenGLRasterizer : IRasterizer
+    {
+        public void SetViewport(int x, int y, int width, int height)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

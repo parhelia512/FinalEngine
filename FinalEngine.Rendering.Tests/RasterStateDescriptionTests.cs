c3af6d0fc44802cee280064852a872d996e6abf2
Software Antics <50978201+softwareantics@users.noreply.github.com>
2020-07-20 20:22:40 +1000

    Unit testing is done.

--- FinalEngine.Rendering.Tests/RasterStateDescriptionTests.cs
+++ FinalEngine.Rendering.Tests/RasterStateDescriptionTests.cs
@@ -42,5 +42,122 @@ namespace FinalEngine.Rendering.Tests
             // Assert
             Assert.IsFalse(state.CullEnabled);
         }
+
+        [Test]
+        public void FaceCullMode_Test_Should_Return_Back_When_Default()
+        {
+            // Arrange
+            RasterStateDescription state = default;
+
+            // Act
+            FaceCullMode actual = state.FaceCullMode;
+
+            // Assert
+            Assert.AreEqual(FaceCullMode.Back, actual);
+        }
+
+        [Test]
+        public void FaceCullMode_Test_Should_Return_Front_When_Property_Set_To_Front()
+        {
+            // Arrange
+            RasterStateDescription state = default;
+
+            // Act
+            state.FaceCullMode = FaceCullMode.Front;
+
+            // Assert
+            Assert.AreEqual(FaceCullMode.Front, state.FaceCullMode);
+        }
+
+        [Test]
+        public void FaceCullMode_Test_Should_Return_Back_When_Property_Set_To_Back()
+        {
+            // Arrange
+            RasterStateDescription state = default;
+
+            // Act
+            state.FaceCullMode = FaceCullMode.Back;
+
+            // Assert
+            Assert.AreEqual(FaceCullMode.Back, state.FaceCullMode);
+        }
+
+        [Test]
+        public void FillMode_Test_Should_Return_Solid_When_Default()
+        {
+            // Arrange
+            RasterStateDescription state = default;
+
+            // Act
+            RasterMode actual = state.FillMode;
+
+            // Assert
+            Assert.AreEqual(RasterMode.Solid, actual);
+        }
+
+        [Test]
+        public void FillMode_Test_Should_Return_Wireframe_When_Property_Set_To_Wireframe()
+        {
+            // Arrange
+            RasterStateDescription state = default;
+
+            // Act
+            state.FillMode = RasterMode.Wireframe;
+
+            // Assert
+            Assert.AreEqual(RasterMode.Wireframe, state.FillMode);
+        }
+
+        [Test]
+        public void FillMode_Test_Should_Return_Solid_When_Property_Set_To_Solid()
+        {
+            // Arrange
+            RasterStateDescription state = default;
+
+            // Act
+            state.FillMode = RasterMode.Solid;
+
+            // Assert
+            Assert.AreEqual(RasterMode.Solid, state.FillMode);
+        }
+
+        [Test]
+        public void WindingDirection_Test_Should_Return_CounterClockwise_When_Default()
+        {
+            // Arrange
+            RasterStateDescription state = default;
+
+            // Act
+            WindingDirection actual = state.WindingDirection;
+
+            // Assert
+            Assert.AreEqual(WindingDirection.CounterClockwise, actual);
+        }
+
+        [Test]
+        public void WindingDirection_Test_Should_Return_CounterClockwise_When_Property_Set_To_CounterClockwise()
+        {
+            // Arrange
+            RasterStateDescription state = default;
+
+            // Act
+            state.WindingDirection = WindingDirection.CounterClockwise;
+
+            // Assert
+            Assert.AreEqual(WindingDirection.CounterClockwise, state.WindingDirection);
+        }
+
+        [Test]
+        public void WindingDirection_Test_Should_Return_Clockwise_When_Property_Set_To_Clockwise()
+        {
+            // Arrange
+            RasterStateDescription state = default;
+
+            // Act
+            state.WindingDirection = WindingDirection.Clockwise;
+
+            // Assert
+            Assert.AreEqual(WindingDirection.Clockwise, state.WindingDirection);
+        }
     }
 }

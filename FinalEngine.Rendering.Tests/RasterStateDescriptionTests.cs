83f9e902a350a5c6dde4b9dbb3f21947cba66cdd
Software Antics <50978201+softwareantics@users.noreply.github.com>
2020-07-20 20:29:07 +1000

    New project no longer generates any warnings.

--- FinalEngine.Rendering.Tests/RasterStateDescriptionTests.cs
+++ FinalEngine.Rendering.Tests/RasterStateDescriptionTests.cs
@@ -1,3 +1,7 @@
+// <copyright file="RasterStateDescriptionTests.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
 namespace FinalEngine.Rendering.Tests
 {
     using NUnit.Framework;

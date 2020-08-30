68bd339e75ed256dfdbb59133e5b6541016507e6
Mathew ODwyer <mathew.odwyer@outlook.com>
2020-08-30 19:11:33 +1000

    Starting on implement windows forms platform and input backend

---
+++ FinalEngine.Platform.Windows/Keyboard/WinFormsKeyboardDevice.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="WinFormsKeyboardDevice.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Platform.Windows.Keyboard
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows.Forms;
+    using FinalEngine.Input.Keyboard;
+    using FEKeyEventArgs = FinalEngine.Input.Keyboard.KeyEventArgs;
+    using WFKeyEventArgs = System.Windows.Forms.KeyEventArgs;
+
+    [ExcludeFromCodeCoverage]
+    public sealed class WinFormsKeyboardDevice : IKeyboardDevice
+    {
+        public WinFormsKeyboardDevice(Control control)
+        {
+            if (control == null)
+            {
+                throw new ArgumentNullException(nameof(control));
+            }
+
+            control.KeyUp += this.Control_KeyUp;
+            control.KeyDown += this.Control_KeyDown;
+        }
+
+        public event EventHandler<FEKeyEventArgs> KeyPressed;
+
+        public event EventHandler<FEKeyEventArgs> KeyReleased;
+
+        private void Control_KeyDown(object sender, WFKeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Control_KeyUp(object sender, WFKeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FinalEngine.Platform.Windows/WinFormsWindow.cs
@@ -0,0 +1,61 @@
+﻿// <copyright file="WinFormsWindow.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Platform.Windows
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows.Forms;
+    using FinalEngine.Drawing;
+    using FinalEngine.Platform;
+
+    [ExcludeFromCodeCoverage]
+    public sealed class WinFormsWindow : Form, IWindow
+    {
+        public WinFormsWindow(int width, int height, string title)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.Title = title;
+
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        Rectangle IWindow.ClientRectangle
+        {
+            get
+            {
+                return new Rectangle(
+                    new Point(this.ClientRectangle.X, this.ClientRectangle.Y),
+                    new Size(this.ClientRectangle.Width, this.ClientRectangle.Height));
+            }
+        }
+
+        Size IWindow.ClientSize
+        {
+            get
+            {
+                return new Size(this.ClientSize.Width, this.ClientSize.Height);
+            }
+        }
+
+        Point IWindow.Location
+        {
+            get { return new Point(this.Location.X, this.Location.Y); }
+            set { this.Location = new System.Drawing.Point(value.X, value.Y); }
+        }
+
+        Size IWindow.Size
+        {
+            get { return new Size(this.Size.Width, this.Size.Height); }
+            set { this.Size = new System.Drawing.Size(value.Width, value.Height); }
+        }
+
+        public string Title
+        {
+            get { return this.Text; }
+            set { this.Text = value; }
+        }
+    }
+}

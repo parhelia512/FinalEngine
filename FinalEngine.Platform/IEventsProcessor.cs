4a97477825b34fa7fe3f29f599d6f4bdf6477d25
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-11-24 15:53:32 +1100

    Create IEventsProcessor

---
+++ FinalEngine.Platform/IEventsProcessor.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="IEventsProcessor.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Platform
+{
+    /// <summary>
+    ///     Defines an interface that provides a method for processing OS-specific events in a
+    ///     message queue.
+    /// </summary>
+    public interface IEventsProcessor
+    {
+        /// <summary>
+        ///     Processes the events that are currently in the message queue.
+        /// </summary>
+        void ProcessEvents();
+    }
+}

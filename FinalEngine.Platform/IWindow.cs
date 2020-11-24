9028bd5b986c50eac5aa818a756cff65f40534bb
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-11-24 15:52:42 +1100

    Create IWindow interface

---
+++ FinalEngine.Platform/IWindow.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="IWindow.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Platform
+{
+    /// <summary>
+    ///     Defines an interface that represents a display or window.
+    /// </summary>
+    public interface IWindow
+    {
+        /// <summary>
+        ///     Gets a value indicating whether this <see cref="IWindow"/> is exiting.
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if this <see cref="IWindow"/> is exiting; otherwise, <c>false</c>.
+        /// </value>
+        bool IsExiting { get; }
+
+        /// <summary>
+        ///     Gets or sets the title of this <see cref="IWindow"/>.
+        /// </summary>
+        /// <value>
+        ///     The title of this <see cref="IWindow"/>.
+        /// </value>
+        string Title { get; set; }
+
+        /// <summary>
+        ///     Gets a value indicating whether this <see cref="IWindow"/> is visible.
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if this <see cref="IWindow"/> is visible; otherwise, <c>false</c>.
+        /// </value>
+        bool Visible { get; }
+    }
+}

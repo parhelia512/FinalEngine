079bb7515fb55d60d8938ccec3f5f0ce7b1a5932
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-11-24 17:20:39 +1100

    Updated documentation

--- FinalEngine.Platform.Desktop/OpenTK/Invocation/INativeWindowInvoker.cs
+++ FinalEngine.Platform.Desktop/OpenTK/Invocation/INativeWindowInvoker.cs
@@ -4,20 +4,60 @@
 
 namespace FinalEngine.Platform.Desktop.OpenTK.Invocation
 {
-    using System;
-
-    public interface INativeWindowInvoker : IDisposable
+    /// <summary>
+    ///     Defines an interface that provides methods for invocation of an OpenTK Native Window.
+    /// </summary>
+    public interface INativeWindowInvoker
     {
+        /// <summary>
+        ///     Gets a value indicating whether this instance is disposed.
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if this instance is disposed; otherwise, <c>false</c>.
+        /// </value>
         bool IsDisposed { get; }
 
+        /// <summary>
+        ///     Gets a value indicating whether the shutdown sequence has been initiated for this
+        ///     window, by calling <see cref="Close"/> or hitting the 'close' button. If this
+        ///     property is true, it is no longer safe to use any OpenTK.Input or
+        ///     OpenTK.Graphics.OpenGL functions or properties.
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if this window is exiting; otherwise, <c>false</c>.
+        /// </value>
         bool IsExiting { get; }
 
+        /// <summary>
+        ///     Gets or sets a value indicating whether this window is visible.
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if this window is visible; otherwise, <c>false</c>.
+        /// </value>
         bool IsVisible { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the title of this window.
+        /// </summary>
+        /// <value>
+        ///     The title of this window.
+        /// </value>
         string Title { get; set; }
 
+        /// <summary>
+        ///     Closes this window.
+        /// </summary>
         void Close();
 
+        /// <summary>
+        ///     Performs application-defined tasks associated with freeing, releasing, or resetting
+        ///     unmanaged resources.
+        /// </summary>
+        void Dispose();
+
+        /// <summary>
+        ///     Processes pending window events.
+        /// </summary>
         void ProcessEvents();
     }
 }
--- FinalEngine.Platform.Desktop/OpenTK/Invocation/NativeWindowInvoker.cs
+++ FinalEngine.Platform.Desktop/OpenTK/Invocation/NativeWindowInvoker.cs
@@ -7,46 +7,70 @@ namespace FinalEngine.Platform.Desktop.OpenTK.Invocation
     using System;
     using global::OpenTK.Windowing.Desktop;
 
-    public class NativeWindowInvoker : INativeWindowInvoker
+    /// <summary>
+    ///     Provides an implementation of an <see cref="INativeWindowInvoker"/>.
+    /// </summary>
+    /// <seealso cref="FinalEngine.Platform.Desktop.OpenTK.Invocation.INativeWindowInvoker"/>
+    public sealed class NativeWindowInvoker : INativeWindowInvoker
     {
+        /// <summary>
+        ///     The native window.
+        /// </summary>
         private readonly NativeWindow nativeWindow;
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="NativeWindowInvoker"/> class.
+        /// </summary>
+        /// <param name="nativeWindow">
+        ///     Specifies a <see cref="NativeWindow"/> that represents the native window whose
+        ///     methods will be invoked.
+        /// </param>
+        /// <exception cref="System.ArgumentNullException">
+        ///     The specified <paramref name="nativeWindow"/> parameter is null.
+        /// </exception>
         public NativeWindowInvoker(NativeWindow nativeWindow)
         {
             this.nativeWindow = nativeWindow ?? throw new ArgumentNullException(nameof(nativeWindow));
         }
 
+        /// <inheritdoc/>
         public bool IsDisposed { get; private set; }
 
+        /// <inheritdoc/>
         public bool IsExiting
         {
             get { return this.nativeWindow.IsExiting; }
         }
 
+        /// <inheritdoc/>
         public bool IsVisible
         {
             get { return this.nativeWindow.IsVisible; }
             set { this.nativeWindow.IsVisible = value; }
         }
 
+        /// <inheritdoc/>
         public string Title
         {
             get { return this.nativeWindow.Title; }
             set { this.nativeWindow.Title = value; }
         }
 
+        /// <inheritdoc/>
         public void Close()
         {
             this.nativeWindow.Close();
             this.IsDisposed = true;
         }
 
+        /// <inheritdoc/>
         public void Dispose()
         {
             this.nativeWindow.Dispose();
             this.IsDisposed = true;
         }
 
+        /// <inheritdoc/>
         public void ProcessEvents()
         {
             this.nativeWindow.ProcessEvents();

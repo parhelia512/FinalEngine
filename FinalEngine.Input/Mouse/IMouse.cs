73c9da7164880ff2888ad603c9214f0d8c363555
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-11-28 02:12:03 +1100

    Create IMouse interface

---
+++ FinalEngine.Input/Mouse/IMouse.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="IMouse.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Input.Mouse
+{
+    public interface IMouse
+    {
+        bool IsButtonDown(MouseButton button);
+
+        bool IsButtonPressed(MouseButton button);
+
+        bool IsButtonReleased(MouseButton button);
+    }
+}

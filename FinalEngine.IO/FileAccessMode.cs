0d0dce19b7f7ac772356b385251a1a0fd1cc00d9
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-11-29 19:33:13 +1100

    Create IFileSystem interface

---
+++ FinalEngine.IO/FileAccessMode.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="FileAccessMode.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.IO
+{
+    public enum FileAccessMode
+    {
+        Read,
+
+        Write,
+
+        ReadAndWrite,
+    }
+}
---
+++ FinalEngine.IO/IFileSystem.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="IFileSystem.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.IO
+{
+    using System.IO;
+
+    public interface IFileSystem
+    {
+        void CreateDirectory(string path);
+
+        Stream CreateFile(string path);
+
+        void DeleteDirectory(string path);
+
+        void DeleteFile(string path);
+
+        bool DirectoryExists(string path);
+
+        bool FileExists(string path);
+
+        Stream OpenFile(string path, FileAccessMode mode);
+    }
+}

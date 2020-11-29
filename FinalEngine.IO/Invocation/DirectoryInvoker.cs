412a7d034927477661d3a6a029abffd7e77160ab
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-11-29 20:08:45 +1100

    Created invoker implementations

---
+++ FinalEngine.IO/Invocation/DirectoryInvoker.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="DirectoryInvoker.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.IO.Invocation
+{
+    using System.IO;
+
+    public class DirectoryInvoker : IDirectoryInvoker
+    {
+        public void CreateDirectory(string path)
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        public void Delete(string path, bool recurse)
+        {
+            Directory.Delete(path, recurse);
+        }
+
+        public bool Exists(string path)
+        {
+            return Directory.Exists(path);
+        }
+    }
+}
---
+++ FinalEngine.IO/Invocation/FileInvoker.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="FileInvoker.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.IO.Invocation
+{
+    using System.IO;
+
+    public class FileInvoker : IFileInvoker
+    {
+        public Stream Create(string path)
+        {
+            return File.Create(path);
+        }
+
+        public void Delete(string path)
+        {
+            File.Delete(path);
+        }
+
+        public bool Exists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public Stream Open(string path, FileMode mode, FileAccess access)
+        {
+            return File.Open(path, mode, access);
+        }
+    }
+}

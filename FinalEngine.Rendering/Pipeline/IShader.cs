c34752b166713e496984463a16f8199d79544e5d
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-12-01 22:48:01 +1100

    Create IShader interface

---
+++ FinalEngine.Rendering/Pipeline/IShader.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="IShader.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Rendering.Pipeline
+{
+    using System;
+
+    public enum PipelineTarget
+    {
+        Vertex,
+
+        Fragment,
+    }
+
+    public interface IShader : IDisposable
+    {
+        PipelineTarget EntryPoint { get; }
+    }
+}

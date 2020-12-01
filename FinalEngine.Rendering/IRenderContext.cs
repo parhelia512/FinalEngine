d5a7e8b69453b193298aab11904f70c1adefaead
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-12-01 13:49:54 +1100

    Create IRenderContext

---
+++ FinalEngine.Rendering/IRenderContext.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="IRenderContext.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Rendering
+{
+    /// <summary>
+    ///   Defines an interface that represents a rendering context.
+    /// </summary>
+    public interface IRenderContext
+    {
+        /// <summary>
+        ///   Swaps the front and back buffers, displaying the rendered scene to the user.
+        /// </summary>
+        void SwapBuffers();
+    }
+}

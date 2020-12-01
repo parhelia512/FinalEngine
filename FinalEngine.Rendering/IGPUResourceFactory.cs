b948cd653327ed3e15b8e292a5b70742814f4e70
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-12-02 00:18:27 +1100

    Create IGPUResourceFactory interface

---
+++ FinalEngine.Rendering/IGPUResourceFactory.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="IGPUResourceFactory.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Rendering
+{
+    using FinalEngine.Rendering.Pipeline;
+
+    public interface IGPUResourceFactory
+    {
+        IShader CreateShader(PipelineTarget target, string sourceCode);
+
+        IShaderProgram CreateShaderProgram(IShader[] shaders);
+    }
+}

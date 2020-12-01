dcd01cd01d9fb938fdd393a28fab9ba22a05dfbe
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-12-01 17:00:57 +1100

    Just setup a single coloured triangle to look at as we develop the rendering API, to make sure everything is working fine.

--- TestGame/Program.cs
+++ TestGame/Program.cs
@@ -6,12 +6,14 @@ namespace TestGame
 {
     using System;
     using System.Drawing;
+    using System.IO;
     using FinalEngine.Input.Keyboard;
     using FinalEngine.Input.Mouse;
     using FinalEngine.IO;
     using FinalEngine.IO.Invocation;
     using FinalEngine.Platform.Desktop.OpenTK;
     using FinalEngine.Platform.Desktop.OpenTK.Invocation;
+    using FinalEngine.Rendering;
     using FinalEngine.Rendering.OpenGL;
     using FinalEngine.Rendering.OpenGL.Invocation;
     using OpenTK.Graphics.OpenGL4;
@@ -68,6 +70,59 @@ namespace TestGame
             var renderContext = new OpenGLRenderContext(opengl, bindings, nativeWindow.Context);
             var renderDevice = new OpenGLRenderDevice(opengl);
 
+            IRasterizer rasterizer = renderDevice.Rasterizer;
+
+            rasterizer.SetRasterState(default);
+
+            int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, File.ReadAllText("Resources\\Shaders\\shader.vert"));
+            GL.CompileShader(vertexShader);
+
+            int pixelShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(pixelShader, File.ReadAllText("Resources\\shaders\\shader.frag"));
+            GL.CompileShader(pixelShader);
+
+            int program = GL.CreateProgram();
+
+            GL.AttachShader(program, vertexShader);
+            GL.AttachShader(program, pixelShader);
+
+            GL.LinkProgram(program);
+            GL.ValidateProgram(program);
+
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(pixelShader);
+
+            float[] vertices =
+            {
+                -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+                0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+                0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+            };
+
+            int[] indices =
+            {
+                0, 1, 2,
+            };
+
+            int vao = GL.GenVertexArray();
+            int vbo = GL.GenBuffer();
+            int ebo = GL.GenBuffer();
+
+            GL.BindVertexArray(vao);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 7 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 7 * sizeof(float), 3 * sizeof(float));
+            GL.EnableVertexAttribArray(1);
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(int), indices, BufferUsageHint.StaticDraw);
+
             while (!window.IsExiting)
             {
                 keyboard.Update();
@@ -76,9 +131,18 @@ namespace TestGame
                 GL.ClearColor(Color.CornflowerBlue);
                 GL.Clear(ClearBufferMask.ColorBufferBit);
 
+                GL.UseProgram(program);
+                GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
+
                 renderContext.SwapBuffers();
                 window.ProcessEvents();
             }
+
+            GL.DeleteBuffer(ebo);
+            GL.DeleteBuffer(vbo);
+            GL.DeleteBuffer(vao);
+
+            GL.DeleteProgram(program);
 
             window.Dispose();
             nativeWindow.Dispose();
---
+++ TestGame/Resources/Shaders/shader.vert
@@ -0,0 +1,13 @@
+﻿#version 460
+
+layout (location = 0) in vec3 in_position;
+layout (location = 1) in vec4 in_color;
+
+layout (location = 0) out vec4 out_color;
+
+void main()
+{
+	out_color = in_color;
+
+	gl_Position = vec4(in_position, 1.0);
+}

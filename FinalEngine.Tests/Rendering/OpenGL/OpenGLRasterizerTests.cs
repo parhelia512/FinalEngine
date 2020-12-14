f1612dd7ca676eab66ef789053c25b68de77e99a
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-12-14 17:13:02 +1100

    Added unit tests for OpenGLRasterizer, only three more classes to go and then I can start working on textures and render targets, think I'll work on basic resource loading and management :)

---
+++ FinalEngine.Tests/Rendering/OpenGL/OpenGLRasterizerTests.cs
@@ -0,0 +1,193 @@
+﻿// <copyright file="OpenGLRasterizerTests.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Tests.Rendering.OpenGL
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Drawing;
+    using FinalEngine.Rendering;
+    using FinalEngine.Rendering.OpenGL;
+    using FinalEngine.Rendering.OpenGL.Invocation;
+    using Moq;
+    using NUnit.Framework;
+    using OpenTK.Graphics.OpenGL4;
+
+    [ExcludeFromCodeCoverage]
+    public class OpenGLRasterizerTests
+    {
+        private Mock<IOpenGLInvoker> invoker;
+
+        private Mock<IEnumMapper> mapper;
+
+        private OpenGLRasterizer rasterizer;
+
+        private RasterStateDescription rasterState;
+
+        [Test]
+        public void ConstructorShouldThrowArgumentNullExceptionWhenInvokerIsNull()
+        {
+            // Arrange, act and assert
+            Assert.Throws<ArgumentNullException>(() => new OpenGLRasterizer(null, this.mapper.Object));
+        }
+
+        [Test]
+        public void ConstructorShouldThrowArgumentNullExceptionWhenMapperIsNull()
+        {
+            // Arrange, act and assert
+            Assert.Throws<ArgumentNullException>(() => new OpenGLRasterizer(this.invoker.Object, null));
+        }
+
+        [Test]
+        public void SetRasterStateShouldInvokeCullFaceWhenInvoked()
+        {
+            // Arrange
+            this.rasterState.CullMode = FaceCullMode.Front;
+
+            // Act
+            this.rasterizer.SetRasterState(this.rasterState);
+
+            // Assert
+            this.invoker.Verify(x => x.CullFace(this.mapper.Object.Forward<CullFaceMode>(FaceCullMode.Front)), Times.Once);
+        }
+
+        [Test]
+        public void SetRasterStateShouldInvokeFrontFaceWhenInvoked()
+        {
+            // Arrange
+            this.rasterState.WindingDirection = WindingDirection.CounterClockwise;
+
+            // Act
+            this.rasterizer.SetRasterState(this.rasterState);
+
+            // Assert
+            this.invoker.Verify(x => x.FrontFace(this.mapper.Object.Forward<FrontFaceDirection>(WindingDirection.CounterClockwise)), Times.Once);
+        }
+
+        [Test]
+        public void SetRasterStateShouldInvokePolygonModeWhenInvoked()
+        {
+            // Arrange
+            this.rasterState.FillMode = RasterMode.Wireframe;
+
+            // Act
+            this.rasterizer.SetRasterState(this.rasterState);
+
+            // Assert
+            this.invoker.Verify(x => x.PolygonMode(MaterialFace.FrontAndBack, this.mapper.Object.Forward<PolygonMode>(RasterMode.Wireframe)), Times.Once);
+        }
+
+        [Test]
+        public void SetRasterStateShouldInvokeSwitchCullFaceFalseWhenCullEnabledIsFalse()
+        {
+            // Arrange
+            this.rasterState.CullEnabled = false;
+
+            // Act
+            this.rasterizer.SetRasterState(this.rasterState);
+
+            // Assert
+            this.invoker.Verify(x => x.Switch(EnableCap.CullFace, false), Times.Once);
+        }
+
+        [Test]
+        public void SetRasterStateShouldInvokeSwitchCullFaceTrueWhenCullEnabledIsTrue()
+        {
+            // Arrange
+            this.rasterState.CullEnabled = true;
+
+            // Act
+            this.rasterizer.SetRasterState(this.rasterState);
+
+            // Assert
+            this.invoker.Verify(x => x.Switch(EnableCap.CullFace, true), Times.Once);
+        }
+
+        [Test]
+        public void SetRasterStateShouldInvokeSwitchScissorTestFalseWhenScissorEnabledIsFalse()
+        {
+            // Arrange
+            this.rasterState.ScissorEnabled = false;
+
+            // Act
+            this.rasterizer.SetRasterState(this.rasterState);
+
+            // Assert
+            this.invoker.Verify(x => x.Switch(EnableCap.ScissorTest, false), Times.Once);
+        }
+
+        [Test]
+        public void SetRasterStateShouldInvokeSwitchScissorTestTrueWhenScissorEnabledIsTrue()
+        {
+            // Arrange
+            this.rasterState.ScissorEnabled = true;
+
+            // Act
+            this.rasterizer.SetRasterState(this.rasterState);
+
+            // Assert
+            this.invoker.Verify(x => x.Switch(EnableCap.ScissorTest, true), Times.Once);
+        }
+
+        [Test]
+        public void SetScissorShouldInvokeScissorWhenInvoked()
+        {
+            // Arrange
+            var rectangle = new Rectangle(10, 54, 364, 251);
+
+            // Act
+            this.rasterizer.SetScissor(rectangle);
+
+            // Assert
+            this.invoker.Verify(x => x.Scissor(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height), Times.Once);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            this.invoker = new Mock<IOpenGLInvoker>();
+            this.mapper = new Mock<IEnumMapper>();
+            this.rasterizer = new OpenGLRasterizer(this.invoker.Object, this.mapper.Object);
+
+            this.rasterState = default;
+        }
+
+        [Test]
+        public void SetViewportShouldInvokeDepthRangeNearZeroFarOneWhenInvoked()
+        {
+            // Act
+            this.rasterizer.SetViewport(Rectangle.Empty);
+
+            // Assert
+            this.invoker.Verify(x => x.DepthRange(0.0f, 1.0f), Times.Once);
+        }
+
+        [Test]
+        public void SetViewportShouldInvokeDepthRangeWhenInvoked()
+        {
+            // Arrange
+            const float Near = 403.0f;
+            const float Far = 159.0f;
+
+            // Act
+            this.rasterizer.SetViewport(Rectangle.Empty, Near, Far);
+
+            // Assert
+            this.invoker.Verify(x => x.DepthRange(Near, Far), Times.Once);
+        }
+
+        [Test]
+        public void SetViewportShouldInvokeViewportWhenInvoked()
+        {
+            // Arrange
+            var rectangle = new Rectangle(124, 543, 5390, 3854);
+
+            // Act
+            this.rasterizer.SetViewport(rectangle);
+
+            // Assert
+            this.invoker.Verify(x => x.Viewport(rectangle), Times.Once);
+        }
+    }
+}

b2065ef9c3cbc533752b23bc268f1e194617f2a7
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-12-22 14:35:58 +1100

    Create IMesh interface

---
+++ FinalEngine.Rendering/IMesh.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IMesh.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Rendering
+{
+    public interface IMesh
+    {
+        void Draw(IRenderDevice renderDevice);
+
+        void Set(IInputAssembler inputAssembler);
+    }
+}

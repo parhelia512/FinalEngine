3be94e069046270a0ab813985373114de4ac5063
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-12-23 18:25:15 +1100

    Added documentation for IRasterizer and OpenGLRasterizer

--- FinalEngine.Rendering.OpenGL/OpenGLRasterizer.cs
+++ FinalEngine.Rendering.OpenGL/OpenGLRasterizer.cs
@@ -10,18 +10,49 @@ namespace FinalEngine.Rendering.OpenGL
     using FinalEngine.Utilities;
     using OpenTK.Graphics.OpenGL4;
 
+    /// <summary>
+    ///   Provides an OpenGL implementation of an <see cref="IRasterizer"/>.
+    /// </summary>
+    /// <seealso cref="FinalEngine.Rendering.IRasterizer"/>
     public class OpenGLRasterizer : IRasterizer
     {
+        /// <summary>
+        ///   The OpenGL invoker.
+        /// </summary>
         private readonly IOpenGLInvoker invoker;
 
+        /// <summary>
+        ///   The OpenGL-to-FinalEngine enumeration mapper.
+        /// </summary>
+        /// <remarks>
+        ///   Used to map OpenGL enumerations to the rendering APIs equivalent.
+        /// </remarks>
         private readonly IEnumMapper mapper;
 
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="OpenGLRasterizer"/> class.
+        /// </summary>
+        /// <param name="invoker">
+        ///   Specifies an <see cref="IOpenGLInvoker"/> that represents the invoker used to invoke OpenGL calls.
+        /// </param>
+        /// <param name="mapper">
+        ///   Specifies an <see cref="IEnumMapper"/> that represents the enumeration mapper used to map OpenGL enumerations to the rendering APIs equivalent.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="invoker"/> or <paramref name="mapper"/> parameter is null.
+        /// </exception>
         public OpenGLRasterizer(IOpenGLInvoker invoker, IEnumMapper mapper)
         {
             this.invoker = invoker ?? throw new ArgumentNullException(nameof(invoker), $"The specified {nameof(invoker)} parameter cannot be null.");
             this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper), $"The specified {nameof(mapper)} parameter cannot be null.");
         }
 
+        /// <summary>
+        ///   Sets the rasterization state of the rasterizer to the specified <paramref name="description"/>.
+        /// </summary>
+        /// <param name="description">
+        ///   Specifies a <see cref="RasterStateDescription"/> that represents the description that defines the rasterization state.
+        /// </param>
         public void SetRasterState(RasterStateDescription description)
         {
             this.invoker.Switch(EnableCap.CullFace, description.CullEnabled);
@@ -31,11 +62,29 @@ namespace FinalEngine.Rendering.OpenGL
             this.invoker.PolygonMode(MaterialFace.FrontAndBack, this.mapper.Forward<PolygonMode>(description.FillMode));
         }
 
+        /// <summary>
+        ///   Sets the scissoring rectangle of the rasterizer to the specified <paramref name="rectangle"/>.
+        /// </summary>
+        /// <param name="rectangle">
+        ///   Specifies a <see cref="Rectangle"/> that represents the rectangle that defines the scissoring rectangle.
+        /// </param>
         public void SetScissor(Rectangle rectangle)
         {
             this.invoker.Scissor(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height);
         }
 
+        /// <summary>
+        ///   Sets the view-port to the specified <paramref name="rectangle"/> and sets the mapping of depth values from normalized device coordinates to window coordinates.
+        /// </summary>
+        /// <param name="rectangle">
+        ///   Specifies a <see cref="Rectangle"/> that represents the rectangle that defines the view-port rectangle.
+        /// </param>
+        /// <param name="near">
+        ///   Specifies a <see cref="float"/> that represents the mapping of the near clipping plane to window coordinates.
+        /// </param>
+        /// <param name="far">
+        ///   Specifies a <see cref="float"/> that represents the mapping of the far clipping plane to window coordinates.
+        /// </param>
         public void SetViewport(Rectangle rectangle, float near = 0.0f, float far = 1.0f)
         {
             this.invoker.Viewport(rectangle);
--- FinalEngine.Rendering/IRasterizer.cs
+++ FinalEngine.Rendering/IRasterizer.cs
@@ -6,12 +6,39 @@ namespace FinalEngine.Rendering
 {
     using System.Drawing;
 
+    /// <summary>
+    ///   Defines an interface that represents the rasterization stage of a graphics pipeline.
+    /// </summary>
     public interface IRasterizer
     {
+        /// <summary>
+        ///   Sets the rasterization state of the rasterizer to the specified <paramref name="description"/>.
+        /// </summary>
+        /// <param name="description">
+        ///   Specifies a <see cref="RasterStateDescription"/> that represents the description that defines the rasterization state.
+        /// </param>
         void SetRasterState(RasterStateDescription description);
 
+        /// <summary>
+        ///   Sets the scissoring rectangle of the rasterizer to the specified <paramref name="rectangle"/>.
+        /// </summary>
+        /// <param name="rectangle">
+        ///   Specifies a <see cref="Rectangle"/> that represents the rectangle that defines the scissoring rectangle.
+        /// </param>
         void SetScissor(Rectangle rectangle);
 
+        /// <summary>
+        ///   Sets the view-port to the specified <paramref name="rectangle"/> and sets the mapping of depth values from normalized device coordinates to window coordinates.
+        /// </summary>
+        /// <param name="rectangle">
+        ///   Specifies a <see cref="Rectangle"/> that represents the rectangle that defines the view-port rectangle.
+        /// </param>
+        /// <param name="near">
+        ///   Specifies a <see cref="float"/> that represents the mapping of the near clipping plane to window coordinates.
+        /// </param>
+        /// <param name="far">
+        ///   Specifies a <see cref="float"/> that represents the mapping of the far clipping plane to window coordinates.
+        /// </param>
         void SetViewport(Rectangle rectangle, float near = 0.0f, float far = 1.0f);
     }
 }

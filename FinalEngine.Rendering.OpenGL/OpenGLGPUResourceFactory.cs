7fb67db1a570a4a671b96c82d22a7bc01b118f09
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-12-23 18:29:05 +1100

    Added documentation for IGPUResourceFactory and OpenGLGPUResourceFactory

--- FinalEngine.Rendering.OpenGL/OpenGLGPUResourceFactory.cs
+++ FinalEngine.Rendering.OpenGL/OpenGLGPUResourceFactory.cs
@@ -19,18 +19,61 @@ namespace FinalEngine.Rendering.OpenGL
     using OpenTK.Graphics.OpenGL4;
     using PixelFormat = FinalEngine.Rendering.Textures.PixelFormat;
 
+    /// <summary>
+    ///   Provides an OpenGL implementation of an <see cref="IGPUResourceFactory"/>.
+    /// </summary>
+    /// <seealso cref="FinalEngine.Rendering.IGPUResourceFactory"/>
     public class OpenGLGPUResourceFactory : IGPUResourceFactory
     {
+        /// <summary>
+        ///   The OpenGL invoker.
+        /// </summary>
         private readonly IOpenGLInvoker invoker;
 
+        /// <summary>
+        ///   The OpenGL-to-FinalEngine enumeration mapper.
+        /// </summary>
+        /// <remarks>
+        ///   Used to map OpenGL enumerations to the rendering APIs equivalent.
+        /// </remarks>
         private readonly IEnumMapper mapper;
 
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="OpenGLGPUResourceFactory"/> class.
+        /// </summary>
+        /// <param name="invoker">
+        ///   Specifies an <see cref="IOpenGLInvoker"/> that represents the invoker used to invoke OpenGL calls.
+        /// </param>
+        /// <param name="mapper">
+        ///   Specifies an <see cref="IEnumMapper"/> that represents the enumeration mapper used to map OpenGL enumerations to the rendering APIs equivalent.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="invoker"/> or <paramref name="mapper"/> parameter is null.
+        /// </exception>
         public OpenGLGPUResourceFactory(IOpenGLInvoker invoker, IEnumMapper mapper)
         {
             this.invoker = invoker ?? throw new ArgumentNullException(nameof(invoker), $"The specified {nameof(invoker)} parameter cannot be null.");
             this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper), $"The specified {nameof(mapper)} parameter cannot be null.");
         }
 
+        /// <summary>
+        ///   Creates an <see cref="IIndexBuffer"/> that contains the specified <paramref name="data"/>.
+        /// </summary>
+        /// <typeparam name="T">
+        ///   Specifies the type of data the <see cref="IIndexBuffer"/> will contain.
+        /// </typeparam>
+        /// <param name="data">
+        ///   Specifies an <see cref="IReadOnlyCollection{T}"/> that represents the data the buffer will contain.
+        /// </param>
+        /// <param name="sizeInBytes">
+        ///   Specifies an <see cref="int"/> that represents the size in bytes of the <paramref name="data"/>.
+        /// </param>
+        /// <returns>
+        ///   The newly created <see cref="IIndexBuffer"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="data"/> parameter is null.
+        /// </exception>
         public IIndexBuffer CreateIndexBuffer<T>(IReadOnlyCollection<T> data, int sizeInBytes)
             where T : struct
         {
@@ -42,6 +85,18 @@ namespace FinalEngine.Rendering.OpenGL
             return new OpenGLIndexBuffer<T>(this.invoker, data, sizeInBytes);
         }
 
+        /// <summary>
+        ///   Creates an <see cref="IInputLayout"/> that formats vertex buffers to the specified <paramref name="elements"/>.
+        /// </summary>
+        /// <param name="elements">
+        ///   Specifies an <see cref="IReadOnlyCollection{InputElement}"/> that represents the elements the input layout will format to when bound.
+        /// </param>
+        /// <returns>
+        ///   The newly created <see cref="IInputLayout"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="elements"/> parameter is null.
+        /// </exception>
         public IInputLayout CreateInputLayout(IReadOnlyCollection<InputElement> elements)
         {
             if (elements == null)
@@ -52,6 +107,21 @@ namespace FinalEngine.Rendering.OpenGL
             return new OpenGLInputLayout(this.invoker, this.mapper, elements);
         }
 
+        /// <summary>
+        ///   Creates an <see cref="IShader"/> targeting the specified <paramref name="target"/>, containing the specified <paramref name="sourceCode"/>.
+        /// </summary>
+        /// <param name="target">
+        ///   Specifies a <see cref="PipelineTarget"/> that represents the entry point of the shader.
+        /// </param>
+        /// <param name="sourceCode">
+        ///   Specifies a <see cref="string"/> that represents the source code of shader.
+        /// </param>
+        /// <returns>
+        ///   The newly created <see cref="IShader"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="sourceCode"/> parameter is null, empty or consists of only whitespace.
+        /// </exception>
         public IShader CreateShader(PipelineTarget target, string sourceCode)
         {
             if (string.IsNullOrWhiteSpace(sourceCode))
@@ -62,6 +132,18 @@ namespace FinalEngine.Rendering.OpenGL
             return new OpenGLShader(this.invoker, this.mapper, this.mapper.Forward<ShaderType>(target), sourceCode);
         }
 
+        /// <summary>
+        ///   Creates an <see cref="IShaderProgram"/> from the specified <paramref name="shaders"/>.
+        /// </summary>
+        /// <param name="shaders">
+        ///   Specifies an <see cref="IReadOnlyCollection{IShader}"/> that represents the collection of shaders used to create the program with.
+        /// </param>
+        /// <returns>
+        ///   The newly created <see cref="IShaderProgram"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="shaders"/> parameter is null.
+        /// </exception>
         public IShaderProgram CreateShaderProgram(IReadOnlyCollection<IShader> shaders)
         {
             if (shaders == null)
@@ -72,6 +154,27 @@ namespace FinalEngine.Rendering.OpenGL
             return new OpenGLShaderProgram(this.invoker, shaders.Cast<IOpenGLShader>().ToList().AsReadOnly());
         }
 
+        /// <summary>
+        ///   Creates an <see cref="ITexture2D"/> from the specified <paramref name="description"/>, containing the specified <paramref name="data"/> and of the specified <paramref name="format"/> and <paramref name="internalFormat"/>.
+        /// </summary>
+        /// <typeparam name="T">
+        ///   Specifies the type of data the texture will contain.
+        /// </typeparam>
+        /// <param name="description">
+        ///   Specifies a <see cref="Texture2DDescription"/> that represents the description of the texture.
+        /// </param>
+        /// <param name="data">
+        ///   Specifies a <see cref="IReadOnlyCollection{T}"/> that represents the data the texture will contain.
+        /// </param>
+        /// <param name="format">
+        ///   Specifies a <see cref="PixelFormat"/> that represents the format of the pixel data.
+        /// </param>
+        /// <param name="internalFormat">
+        ///   Specifies a <see cref="SizedFormat"/> that represents the sized internal format to be used to store the texture image data.
+        /// </param>
+        /// <returns>
+        ///   The newly created <see cref="ITexture2D"/>.
+        /// </returns>
         public ITexture2D CreateTexture2D<T>(Texture2DDescription description, IReadOnlyCollection<T>? data, PixelFormat format = PixelFormat.Rgba, SizedFormat internalFormat = SizedFormat.Rgba8)
         {
             var handle = GCHandle.Alloc(data, GCHandleType.Pinned);
@@ -84,6 +187,27 @@ namespace FinalEngine.Rendering.OpenGL
             return result;
         }
 
+        /// <summary>
+        ///   Creates an <see cref="IVertexBuffer"/> that contains the specified <paramref name="data"/>.
+        /// </summary>
+        /// <typeparam name="T">
+        ///   The type of data the buffer will contain.
+        /// </typeparam>
+        /// <param name="data">
+        ///   Specifies an <see cref="IReadOnlyCollection{T}"/> that represents the data the buffer will contain.
+        /// </param>
+        /// <param name="sizeInBytes">
+        ///   Specifies an <see cref="int"/> that represents the size in bytes of the data.
+        /// </param>
+        /// <param name="stride">
+        ///   Specifies an <see cref="int"/> that represents the total stride of one vertex within the buffer.
+        /// </param>
+        /// <returns>
+        ///   The newly created <see cref="IVertexBuffer"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="data"/> parameter is null.
+        /// </exception>
         public IVertexBuffer CreateVertexBuffer<T>(IReadOnlyCollection<T> data, int sizeInBytes, int stride)
             where T : struct
         {
--- FinalEngine.Rendering/IGPUResourceFactory.cs
+++ FinalEngine.Rendering/IGPUResourceFactory.cs
@@ -4,24 +4,127 @@
 
 namespace FinalEngine.Rendering
 {
+    using System;
     using System.Collections.Generic;
     using FinalEngine.Rendering.Buffers;
     using FinalEngine.Rendering.Pipeline;
     using FinalEngine.Rendering.Textures;
 
+    /// <summary>
+    ///   Defines an interface that provides methods for creating GPU resources.
+    /// </summary>
     public interface IGPUResourceFactory
     {
+        /// <summary>
+        ///   Creates an <see cref="IIndexBuffer"/> that contains the specified <paramref name="data"/>.
+        /// </summary>
+        /// <typeparam name="T">
+        ///   Specifies the type of data the <see cref="IIndexBuffer"/> will contain.
+        /// </typeparam>
+        /// <param name="data">
+        ///   Specifies an <see cref="IReadOnlyCollection{T}"/> that represents the data the buffer will contain.
+        /// </param>
+        /// <param name="sizeInBytes">
+        ///   Specifies an <see cref="int"/> that represents the size in bytes of the <paramref name="data"/>.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="data"/> parameter is null.
+        /// </exception>
+        /// <returns>
+        ///   The newly created <see cref="IIndexBuffer"/>.
+        /// </returns>
         IIndexBuffer CreateIndexBuffer<T>(IReadOnlyCollection<T> data, int sizeInBytes)
             where T : struct;
 
+        /// <summary>
+        ///   Creates an <see cref="IInputLayout"/> that formats vertex buffers to the specified <paramref name="elements"/>.
+        /// </summary>
+        /// <param name="elements">
+        ///   Specifies an <see cref="IReadOnlyCollection{InputElement}"/> that represents the elements the input layout will format to when bound.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="elements"/> parameter is null.
+        /// </exception>
+        /// <returns>
+        ///   The newly created <see cref="IInputLayout"/>.
+        /// </returns>
         IInputLayout CreateInputLayout(IReadOnlyCollection<InputElement> elements);
 
+        /// <summary>
+        ///   Creates an <see cref="IShader"/> targeting the specified <paramref name="target"/>, containing the specified <paramref name="sourceCode"/>.
+        /// </summary>
+        /// <param name="target">
+        ///   Specifies a <see cref="PipelineTarget"/> that represents the entry point of the shader.
+        /// </param>
+        /// <param name="sourceCode">
+        ///   Specifies a <see cref="string"/> that represents the source code of shader.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="sourceCode"/> parameter is null, empty or consists of only white-space.
+        /// </exception>
+        /// <returns>
+        ///   The newly created <see cref="IShader"/>.
+        /// </returns>
         IShader CreateShader(PipelineTarget target, string sourceCode);
 
+        /// <summary>
+        ///   Creates an <see cref="IShaderProgram"/> from the specified <paramref name="shaders"/>.
+        /// </summary>
+        /// <param name="shaders">
+        ///   Specifies an <see cref="IReadOnlyCollection{IShader}"/> that represents the collection of shaders used to create the program with.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="shaders"/> parameter is null.
+        /// </exception>
+        /// <returns>
+        ///   The newly created <see cref="IShaderProgram"/>.
+        /// </returns>
         IShaderProgram CreateShaderProgram(IReadOnlyCollection<IShader> shaders);
 
+        /// <summary>
+        ///   Creates an <see cref="ITexture2D"/> from the specified <paramref name="description"/>, containing the specified <paramref name="data"/> and of the specified <paramref name="format"/> and <paramref name="internalFormat"/>.
+        /// </summary>
+        /// <typeparam name="T">
+        ///   Specifies the type of data the texture will contain.
+        /// </typeparam>
+        /// <param name="description">
+        ///   Specifies a <see cref="Texture2DDescription"/> that represents the description of the texture.
+        /// </param>
+        /// <param name="data">
+        ///   Specifies a <see cref="IReadOnlyCollection{T}"/> that represents the data the texture will contain.
+        /// </param>
+        /// <param name="format">
+        ///   Specifies a <see cref="PixelFormat"/> that represents the format of the pixel data.
+        /// </param>
+        /// <param name="internalFormat">
+        ///   Specifies a <see cref="SizedFormat"/> that represents the sized internal format to be used to store the texture image data.
+        /// </param>
+        /// <returns>
+        ///   The newly created <see cref="ITexture2D"/>.
+        /// </returns>
         ITexture2D CreateTexture2D<T>(Texture2DDescription description, IReadOnlyCollection<T>? data, PixelFormat format = PixelFormat.Rgba, SizedFormat internalFormat = SizedFormat.Rgba8);
 
+        /// <summary>
+        ///   Creates an <see cref="IVertexBuffer"/> that contains the specified <paramref name="data"/>.
+        /// </summary>
+        /// <typeparam name="T">
+        ///   Specifies the type of data the buffer will contain.
+        /// </typeparam>
+        /// <param name="data">
+        ///   Specifies an <see cref="IReadOnlyCollection{T}"/> that represents the data the buffer will contain.
+        /// </param>
+        /// <param name="sizeInBytes">
+        ///   Specifies an <see cref="int"/> that represents the size in bytes of the data.
+        /// </param>
+        /// <param name="stride">
+        ///   Specifies an <see cref="int"/> that represents the total stride of one vertex within the buffer.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="data"/> parameter is null.
+        /// </exception>
+        /// <returns>
+        ///   The newly created <see cref="IVertexBuffer"/>.
+        /// </returns>
         IVertexBuffer CreateVertexBuffer<T>(IReadOnlyCollection<T> data, int sizeInBytes, int stride)
                             where T : struct;
     }

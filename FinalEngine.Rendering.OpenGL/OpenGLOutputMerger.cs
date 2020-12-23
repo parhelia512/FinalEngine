a7b6ce4e8613b5559d4bf77f50abf858cb81be1a
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2020-12-23 18:26:48 +1100

    Added documentation for IOutputMerger and OpenGLOutputMerger

--- FinalEngine.Rendering.OpenGL/OpenGLOutputMerger.cs
+++ FinalEngine.Rendering.OpenGL/OpenGLOutputMerger.cs
@@ -9,18 +9,49 @@ namespace FinalEngine.Rendering.OpenGL
     using FinalEngine.Utilities;
     using OpenTK.Graphics.OpenGL4;
 
+    /// <summary>
+    ///   Provides an OpenGL implementation of an <see cref="IOutputMerger"/>.
+    /// </summary>
+    /// <seealso cref="FinalEngine.Rendering.IOutputMerger"/>
     public class OpenGLOutputMerger : IOutputMerger
     {
+        /// <summary>
+        ///   The OpenGL invoker.
+        /// </summary>
         private readonly IOpenGLInvoker invoker;
 
+        /// <summary>
+        ///   The OpenGL-to-FinalEngine enumeration mapper.
+        /// </summary>
+        /// <remarks>
+        ///   Used to map OpenGL enumerations to the rendering APIs equivalent.
+        /// </remarks>
         private readonly IEnumMapper mapper;
 
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="OpenGLOutputMerger"/> class.
+        /// </summary>
+        /// <param name="invoker">
+        ///   Specifies an <see cref="IOpenGLInvoker"/> that represents the invoker used to invoke OpenGL calls.
+        /// </param>
+        /// <param name="mapper">
+        ///   Specifies an <see cref="IEnumMapper"/> that represents the enumeration mapper used to map OpenGL enumerations to the rendering APIs equivalent.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="invoker"/> or <paramref name="mapper"/> parameter is null.
+        /// </exception>
         public OpenGLOutputMerger(IOpenGLInvoker invoker, IEnumMapper mapper)
         {
             this.invoker = invoker ?? throw new ArgumentNullException(nameof(invoker), $"The specified {nameof(invoker)} parameter cannot be null.");
             this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper), $"The specified {nameof(mapper)} parameter cannot be null.");
         }
 
+        /// <summary>
+        ///   Sets the blend state of the output merger to the specified <paramref name="description"/>.
+        /// </summary>
+        /// <param name="description">
+        ///   Specifies a <see cref="BlendStateDescription"/> that represents the description that defines the blend state.
+        /// </param>
         public void SetBlendState(BlendStateDescription description)
         {
             this.invoker.Switch(EnableCap.Blend, description.Enabled);
@@ -29,6 +60,12 @@ namespace FinalEngine.Rendering.OpenGL
             this.invoker.BlendFunc(this.mapper.Forward<BlendingFactor>(description.SourceMode), this.mapper.Forward<BlendingFactor>(description.DestinationMode));
         }
 
+        /// <summary>
+        ///   Sets the depth state of the output merger to the specified <paramref name="description"/>.
+        /// </summary>
+        /// <param name="description">
+        ///   Specifies a <see cref="DepthStateDescription"/> that represents the description that defines the depth state.
+        /// </param>
         public void SetDepthState(DepthStateDescription description)
         {
             this.invoker.Switch(EnableCap.DepthTest, description.ReadEnabled);
@@ -36,6 +73,12 @@ namespace FinalEngine.Rendering.OpenGL
             this.invoker.DepthFunc(this.mapper.Forward<DepthFunction>(description.ComparisonMode));
         }
 
+        /// <summary>
+        ///   Sets the stencil state of the output merger to the specified <paramref name="description"/>.
+        /// </summary>
+        /// <param name="description">
+        ///   Specifies a <see cref="StencilStateDescription"/> that represents the description that defines the stencil state.
+        /// </param>
         public void SetStencilState(StencilStateDescription description)
         {
             this.invoker.Switch(EnableCap.StencilTest, description.Enabled);
--- FinalEngine.Rendering/IOutputMerger.cs
+++ FinalEngine.Rendering/IOutputMerger.cs
@@ -4,12 +4,33 @@
 
 namespace FinalEngine.Rendering
 {
+    /// <summary>
+    ///   Defines an interface that represents the output-merging stage of a graphics pipeline.
+    /// </summary>
     public interface IOutputMerger
     {
+        /// <summary>
+        ///   Sets the blend state of the output merger to the specified <paramref name="description"/>.
+        /// </summary>
+        /// <param name="description">
+        ///   Specifies a <see cref="BlendStateDescription"/> that represents the description that defines the blend state.
+        /// </param>
         void SetBlendState(BlendStateDescription description);
 
+        /// <summary>
+        ///   Sets the depth state of the output merger to the specified <paramref name="description"/>.
+        /// </summary>
+        /// <param name="description">
+        ///   Specifies a <see cref="DepthStateDescription"/> that represents the description that defines the depth state.
+        /// </param>
         void SetDepthState(DepthStateDescription description);
 
+        /// <summary>
+        ///   Sets the stencil state of the output merger to the specified <paramref name="description"/>.
+        /// </summary>
+        /// <param name="description">
+        ///   Specifies a <see cref="StencilStateDescription"/> that represents the description that defines the stencil state.
+        /// </param>
         void SetStencilState(StencilStateDescription description);
     }
 }

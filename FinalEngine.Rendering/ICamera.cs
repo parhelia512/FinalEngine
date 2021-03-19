2c82179780760bcc8ad4942e9bfe5ee50de24936
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2021-03-19 18:20:46 +1100

    Good to merge.

--- FinalEngine.Rendering/ICamera.cs
+++ FinalEngine.Rendering/ICamera.cs
@@ -6,10 +6,25 @@ namespace FinalEngine.Rendering
 {
     using System.Numerics;
 
+    /// <summary>
+    ///   Defines an interface that represents a camera.
+    /// </summary>
     public interface ICamera
     {
+        /// <summary>
+        ///   Gets the projection matrix of the camera.
+        /// </summary>
+        /// <value>
+        ///   The projection matrix.
+        /// </value>
         Matrix4x4 Projection { get; }
 
+        /// <summary>
+        ///   Gets the view matrix of the camera.
+        /// </summary>
+        /// <value>
+        ///   The view matrix.
+        /// </value>
         Matrix4x4 View { get; }
     }
 }

4395428acdb11d0b249df560db06de1c7d492f73
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2021-03-19 17:02:24 +1100

    Create ICamera interface

---
+++ FinalEngine.Rendering/ICamera.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="ICamera.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Rendering
+{
+    using System.Numerics;
+
+    public interface ICamera
+    {
+        Matrix4x4 Projection { get; }
+
+        Matrix4x4 View { get; }
+    }
+}
